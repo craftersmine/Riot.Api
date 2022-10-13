4d2e24b427de10ada798a49698855f4ddf12d3b6
craftersmine <rodion.morozov.01@gmail.com>
2022-10-13 20:09:15 +0700

    Added Region and Platform enums

---
+++ craftersmine.Riot.Api.Common/RiotPlatform.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Common
+{
+    public enum RiotPlatform
+    {
+        Brazil,
+        EuropeNordicEast,
+        EuropeWest,
+        LatinAmericaNorth,
+        LatinAmericaSouth,
+        NorthAmerica,
+        Oceania,
+        Russia,
+        Turkey,
+        Japan,
+        Korea,
+    }
+
+    public static class RiotPlatformExtensions
+    {
+        public static string GetAddressFor(this RiotPlatform platform)
+        {
+            switch (platform)
+            {
+                case RiotPlatform.Brazil:
+                    return Constants.BaseAddressPlatformBr1;
+                case RiotPlatform.EuropeNordicEast:
+                    return Constants.BaseAddressPlatformEun1;
+                case RiotPlatform.EuropeWest:
+                    return Constants.BaseAddressPlatformEuw1;
+                case RiotPlatform.LatinAmericaNorth:
+                    return Constants.BaseAddressPlatformLa1;
+                case RiotPlatform.LatinAmericaSouth:
+                    return Constants.BaseAddressPlatformLa2;
+                case RiotPlatform.NorthAmerica:
+                    return Constants.BaseAddressPlatformNa1;
+                case RiotPlatform.Oceania:
+                    return Constants.BaseAddressPlatformOc1;
+                case RiotPlatform.Russia:
+                    return Constants.BaseAddressPlatformRu;
+                case RiotPlatform.Turkey:
+                    return Constants.BaseAddressPlatformTr1;
+                case RiotPlatform.Japan:
+                    return Constants.BaseAddressPlatformJp1;
+                case RiotPlatform.Korea:
+                    return Constants.BaseAddressPlatformKr;
+                default:
+                    throw new Exception("Something went wrong when selecting platform! This should not happen");
+            }
+        }
+    }
+}
---
+++ craftersmine.Riot.Api.Common/RiotRegion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Common
+{
+    public enum RiotRegion
+    {
+        Americas,
+        Asia,
+        Europe,
+        Oceania,
+        Esports
+    }
+
+    public static class RiotRegionExtensions
+    {
+        public static string GetAddressFor(this RiotRegion region)
+        {
+            switch (region)
+            {
+                case RiotRegion.Americas:
+                    return Constants.BaseAddressRegionAmerica;
+                case RiotRegion.Asia:
+                    return Constants.BaseAddressRegionAsia;
+                case RiotRegion.Europe:
+                    return Constants.BaseAddressRegionEurope;
+                case RiotRegion.Oceania:
+                    return Constants.BaseAddressRegionSea;
+                case RiotRegion.Esports:
+                    return Constants.BaseAddressRegionEsports;
+                default:
+                    throw new Exception("Something went wrong with selecting region! This should not happen");
+            }
+        }
+    }
+}
