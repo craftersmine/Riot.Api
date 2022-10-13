acfd57d186bd067949797a1212121a276a82cbf9
craftersmine <rodion.morozov.01@gmail.com>
2022-10-14 01:27:41 +0700

    Updated Tests

---
+++ craftersmine.Riot.Api.Tests/Account/RiotAccountApiClientTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.Account;
+using craftersmine.Riot.Api.Common;
+
+namespace craftersmine.Riot.Api.Tests.Account
+{
+    [TestClass]
+    public class RiotAccountApiClientTests
+    {
+        public const string MyPuuid = "XRWOl9NePvqBbDsCBQKMgks13Cyc5KO1N-ZCPE0xr8Yvt58H7JjiWx_jlkF4VSYc31kBYfoKZtSNhA";
+        public const string MyGameName = "craftersmine";
+        public const string MyTagLine = "Ornn";
+        public const RiotShards MyShardLor = RiotShards.LoREurope;
+        public const RiotShards MyShardValorant = RiotShards.ValorantEurope;
+
+        public TestContext TestContext { get; set; }
+
+        public static string ApiKey;
+        public static RiotAccountApiClient Client;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ApiKey = TestContext.Properties["ApiKey"].ToString();
+            if (string.IsNullOrWhiteSpace(ApiKey))
+                Assert.Fail("No Riot API key provided!");
+            Client = new RiotAccountApiClient(new RiotApiClientSettingsBuilder().UseApiKey(ApiKey)
+                .UseDefaultDataRegion(RiotRegion.Europe).Build());
+        }
+
+        [TestMethod]
+        public async Task GetAccountByPuuidTest()
+        {
+            RiotAccount account = await Client.GetAccountByPuuidAsync(MyPuuid);
+            Assert.IsNotNull(account);
+            Assert.AreEqual(MyPuuid, account.Puuid);
+            Assert.AreEqual(MyGameName, account.RiotId);
+            Assert.AreEqual(MyTagLine, account.RiotIdTag);
+        }
+
+        [TestMethod]
+        public async Task GetAccountByRiotIdTest()
+        {
+            RiotAccount account = await Client.GetAccountByRiotIdAsync(MyGameName, MyTagLine);
+            Assert.IsNotNull(account);
+            Assert.AreEqual(MyPuuid, account.Puuid);
+            Assert.AreEqual(MyGameName, account.RiotId);
+            Assert.AreEqual(MyTagLine, account.RiotIdTag);
+        }
+
+        [TestMethod]
+        public async Task GetActiveShardByPuuidTest()
+        {
+            RiotActiveShard shard = await Client.GetActiveShardForPlayerByPuuidAsync(RiotShardGame.Valorant, MyPuuid);
+            Assert.IsNotNull(shard);
+            Assert.AreEqual(shard.ActiveShard, MyShardValorant);
+            Assert.AreEqual(shard.Game, RiotShardGame.Valorant);
+            Assert.AreEqual(shard.Puuid, MyPuuid);
+            shard = await Client.GetActiveShardForPlayerByPuuidAsync(RiotShardGame.LegendsOfRunterra, MyPuuid);
+            Assert.IsNotNull(shard);
+            Assert.AreEqual(shard.ActiveShard, MyShardLor);
+            Assert.AreEqual(shard.Game, RiotShardGame.LegendsOfRunterra);
+            Assert.AreEqual(shard.Puuid, MyPuuid);
+        }
+    }
+}
