082f0fb5c23c747def98fc5e64c6277ce4e47f7e
craftersmine <rodion.morozov.01@gmail.com>
2022-10-13 22:19:30 +0700

    Added Account API project

---
+++ craftersmine.Riot.Api.Account/RiotAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Account
+{
+    /// <summary>
+    /// Represents a Riot Account
+    /// </summary>
+    public class RiotAccount
+    {
+        /// <summary>
+        /// Gets Riot Account PUUID
+        /// </summary>
+        [JsonProperty("puuid")]
+        public string Puuid { get; private set; }
+        /// <summary>
+        /// Gets Riot Account name (value before # symbol)
+        /// </summary>
+        [JsonProperty("gameName")]
+        public string GameName { get; private set; }
+        /// <summary>
+        /// Gets Riot Account tag (value after # symbol)
+        /// </summary>
+        [JsonProperty("tagLine")]
+        public string TagLine { get; private set; }
+    }
+}
---
+++ craftersmine.Riot.Api.Account/RiotAccountApiClient.cs
@@ -0,0 +1,27 @@
+﻿using craftersmine.Riot.Api.Common;
+
+namespace craftersmine.Riot.Api.Account
+{
+    /// <summary>
+    /// Represents Account v1 Riot API
+    /// </summary>
+    public class RiotAccountApiClient : RiotApiClient
+    {
+        private const string _apiEndpointRoot = "/riot/account/v1/accounts/";
+
+        public RiotAccountApiClient(RiotApiClientSettings settings) : base(settings) { }
+
+        public async Task<RiotAccount> GetByPuuid(string puuid)
+        {
+            if (string.IsNullOrWhiteSpace(puuid))
+                throw new ArgumentNullException(nameof(puuid));
+
+            string endpoint = UriUtils.JoinEndpoints(_apiEndpointRoot, "by-puuid", puuid);
+
+            RiotAccount account =
+                await Client.Get<RiotAccount>(UriUtils.GetAddress(Settings.DefaultDataRegion, endpoint), null);
+
+            return account;
+        }
+    }
+}
