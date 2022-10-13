aee08b5316911051f0bfc434ac8e83d22e0b45f7
craftersmine <rodion.morozov.01@gmail.com>
2022-10-13 20:08:36 +0700

    Added RiotApiClient base class

---
+++ craftersmine.Riot.Api.Common/RiotApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Common
+{
+    public class RiotApiClient
+    {
+        private Client _client;
+        private RiotApiClientSettings _settings;
+
+        public RiotApiClient(RiotApiClientSettings settings)
+        {
+            _client = new Client();
+        }
+    }
+}
