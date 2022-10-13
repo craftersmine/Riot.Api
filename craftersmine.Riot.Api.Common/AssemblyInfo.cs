253df7f802c22a79b83072c5a87bd69e9b261b5b
craftersmine <rodion.morozov.01@gmail.com>
2022-10-13 22:19:55 +0700

    Updated project related stuff

--- craftersmine.Riot.Api.Common/AssemblyInfo.cs
+++ craftersmine.Riot.Api.Common/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.CompilerServices;
 using System.Text;
 
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.Tests")]
+[assembly:InternalsVisibleTo("craftersmine.Riot.Api.Account")]
