48ccada1d429515b9118896f0837e78c268e46a3
craftersmine <rodion.morozov.01@gmail.com>
2022-10-14 13:33:40 +0700

    Added Summoner v4 API

---
+++ craftersmine.Riot.Api.League.Summoner/LeagueSummoner.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace craftersmine.Riot.Api.League.Summoner
+{
+    /// <summary>
+    /// Represents a League of Legends summoner account
+    /// </summary>
+    public class LeagueSummoner
+    {
+        /// <summary>
+        /// Gets an encrypted account ID. Max length is 56 characters
+        /// </summary>
+        [JsonProperty("accountId")]
+        public string? AccountId { get; private set; }
+        /// <summary>
+        /// Gets a League of Legends summoner account name
+        /// </summary>
+        [JsonProperty("name")]
+        public string? Name { get; private set; }
+        /// <summary>
+        /// Gets an encrypted summoner ID. Max length is 63 characters
+        /// </summary>
+        [JsonProperty("id")]
+        public string? Id { get; private set; }
+        /// <summary>
+        /// Gets a Riot Account PUUID
+        /// </summary>
+        [JsonProperty("puuid")]
+        public string? RiotPuuid { get; private set; }
+        /// <summary>
+        /// Gets an ID of League of Legends summoner profile icon
+        /// </summary>
+        [JsonProperty("profileIconId")]
+        public int ProfileIconId { get; private set; }
+        /// <summary>
+        /// Gets a League of Legends summoner level
+        /// </summary>
+        [JsonProperty("summonerLevel")]
+        public long SummonerLevel { get; private set; }
+        /// <summary>
+        /// Gets a <see cref="DateTime"/> in GMT timezone when League of Legends summoner account was modified (summoner name change, level change or profile icon change
+        /// </summary>
+        [JsonProperty("revisionDate"), JsonConverter(typeof(craftersmine.Riot.Api.Common.Converters.UnixDateTimeConverter))]
+        public DateTime RevisionDate { get; private set; }
+    }
+}
---
+++ craftersmine.Riot.Api.League.Summoner/RiotLeagueSummonerApiClient.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.Common;
+using craftersmine.Riot.Api.Common.Exceptions;
+
+namespace craftersmine.Riot.Api.League.Summoner
+{
+    /// <summary>
+    /// Represents Riot League of Legends Summoner v4 API
+    /// </summary>
+    public class RiotLeagueSummonerApiClient : RiotApiClient
+    {
+        private const string _apiEndpointRoot = "/lol/summoner/v4/summoners/";
+
+        /// <summary>
+        /// Creates new instance of <see cref="RiotLeagueSummonerApiClient"/> with specified settings
+        /// </summary>
+        /// <param name="settings"></param>
+        public RiotLeagueSummonerApiClient(RiotApiClientSettings settings) : base(settings) { }
+
+        /// <summary>
+        /// Gets a League of legends Summoner account with specified summoner name in specified region
+        /// </summary>
+        /// <param name="region">League of Legends server region</param>
+        /// <param name="summonerName">League of Legends summoner name</param>
+        /// <returns><see cref="LeagueSummoner"/> object</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="RiotApiException"></exception>
+        public async Task<LeagueSummoner> GetSummonerByNameAsync(RiotPlatform region, string summonerName)
+        {
+            if (string.IsNullOrWhiteSpace(summonerName))
+                throw new ArgumentNullException(nameof(summonerName));
+
+            string endpoint =
+                UriUtils.GetAddress(region, UriUtils.JoinEndpoints(_apiEndpointRoot, "by-name", summonerName));
+
+            LeagueSummoner summoner = await Client.Get<LeagueSummoner>(endpoint, null);
+            return summoner;
+        }
+        
+        /// <summary>
+        /// Gets a League of legends Summoner account with specified Riot Account PUUID in specified region
+        /// </summary>
+        /// <param name="region">League of Legends server region</param>
+        /// <param name="puuid">Riot Account PUUID</param>
+        /// <returns><see cref="LeagueSummoner"/> object</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="RiotApiException"></exception>
+        public async Task<LeagueSummoner> GetSummonerByPuuidAsync(RiotPlatform region, string puuid)
+        {
+            if (string.IsNullOrWhiteSpace(puuid))
+                throw new ArgumentNullException(nameof(puuid));
+
+            string endpoint = UriUtils.GetAddress(region, UriUtils.JoinEndpoints(_apiEndpointRoot, "by-puuid", puuid));
+
+            LeagueSummoner summoner = await Client.Get<LeagueSummoner>(endpoint, null);
+            return summoner;
+        }
+        
+        /// <summary>
+        /// Gets a League of legends Summoner account with specified account ID in specified region
+        /// </summary>
+        /// <param name="region">League of Legends server region</param>
+        /// <param name="puuid">Game account ID</param>
+        /// <returns><see cref="LeagueSummoner"/> object</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="RiotApiException"></exception>
+        public async Task<LeagueSummoner> GetSummonerByAccountIdAsync(RiotPlatform region, string accountId)
+        {
+            if (string.IsNullOrWhiteSpace(accountId))
+                throw new ArgumentNullException(nameof(accountId));
+
+            string endpoint =
+                UriUtils.GetAddress(region, UriUtils.JoinEndpoints(_apiEndpointRoot, "by-account", accountId));
+
+            LeagueSummoner summoner = await Client.Get<LeagueSummoner>(endpoint, null);
+            return summoner;
+        }
+        
+        /// <summary>
+        /// Gets a League of legends Summoner account with specified League of Legends Summoner ID in specified region
+        /// </summary>
+        /// <param name="region">League of Legends server region</param>
+        /// <param name="puuid">League of Legends summoner ID</param>
+        /// <returns><see cref="LeagueSummoner"/> object</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="RiotApiException"></exception>
+        public async Task<LeagueSummoner> GetSummonerByIdAsync(RiotPlatform region, string summonerId)
+        {
+            if (string.IsNullOrWhiteSpace(summonerId))
+                throw new ArgumentNullException(nameof(summonerId));
+
+            string endpoint = UriUtils.GetAddress(region, UriUtils.JoinEndpoints(_apiEndpointRoot, summonerId));
+
+            LeagueSummoner summoner = await Client.Get<LeagueSummoner>(endpoint, null);
+            return summoner;
+        }
+    }
+}
