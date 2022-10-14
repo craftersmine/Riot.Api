5b1934c3b64b381d35f9bd40d73817e41d065137
craftersmine <rodion.morozov.01@gmail.com>
2022-10-14 13:32:26 +0700

    Added UnixDateTimeConverter for JSON

---
+++ craftersmine.Riot.Api.Common/Converters/UnixDateTimeConverter.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Common.Converters
+{
+    internal class UnixDateTimeConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            long val;
+            if (value is DateTime)
+            {
+                val = ((DateTime)value).ToUnixTime();
+            }
+            else
+            {
+                throw new Exception("Expected DateTime value, got " + value.GetType().Name);
+            }
+            writer.WriteValue(val);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+                return new DateTime(1970, 1, 1);
+
+            if (reader.TokenType != JsonToken.Integer)
+                throw new Exception("Expected integer value, got " + reader.TokenType);
+
+            long ticks = (long)(reader.Value ?? 0);
+            return ticks.FromUnixTime();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(int);
+        }
+    }
+}
---
+++ craftersmine.Riot.Api.Common/UnixDateTimeUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Common
+{
+    internal static class UnixTimeHelper
+    {
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1);
+
+        public static DateTime FromUnixTime(this long val)
+        {
+            return UnixEpoch.AddMilliseconds(val);
+        }
+
+        public static long ToUnixTime(this DateTime val)
+        {
+            if (val == DateTime.MinValue)
+                return 0;
+
+            var delta = val - UnixEpoch;
+
+            if (delta.TotalSeconds < 0)
+                throw new ArgumentOutOfRangeException(nameof(val), val, "Invalid datetime!");
+
+            return (long) delta.TotalSeconds;
+        }
+    }
+}
