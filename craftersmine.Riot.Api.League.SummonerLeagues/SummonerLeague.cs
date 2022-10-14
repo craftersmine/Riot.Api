90ecda03aa330cfd6d5e046655bb87f50d9b2003
craftersmine <rodion.morozov.01@gmail.com>
2022-10-15 03:24:13 +0700

    Implemented League Summoner class

--- craftersmine.Riot.Api.League.SummonerLeagues/SummonerLeague.cs
+++ craftersmine.Riot.Api.League.SummonerLeagues/SummonerLeague.cs
@@ -33,7 +33,45 @@ namespace craftersmine.Riot.Api.League.SummonerLeagues
         /// </summary>
         [JsonProperty("queueType"), JsonConverter(typeof(LeagueQueueTypeConverter))]
         public LeagueQueueType LeagueQueueType { get; private set; }
-
-
+        /// <summary>
+        /// Gets League of Legends summoner ranked tier
+        /// </summary>
+        [JsonProperty("tier"), JsonConverter(typeof(LeagueRankedTierConverter))]
+        public LeagueRankedTier Tier { get; private set; }
+        /// <summary>
+        /// Gets League of Legends summoner ranked LP value
+        /// </summary>
+        [JsonProperty("leaguePoints")]
+        public int LeaguePoints { get; private set; }
+        /// <summary>
+        /// Gets League of Legends summoner win count
+        /// </summary>
+        [JsonProperty("wins")]
+        public int Wins { get; private set; }
+        /// <summary>
+        /// Gets League of Legends summoner loss count
+        /// </summary>
+        [JsonProperty("losses")]
+        public int Losses { get; private set; }
+        /// <summary>
+        /// Gets <see langword="true"/> if League of Legends summoner has a hot streak (win streak)
+        /// </summary>
+        [JsonProperty("hotStreak")]
+        public bool IsHotStreak { get; private set; }
+        /// <summary>
+        /// Gets <see langword="true"/> if League of Legends summoner is a veteran player
+        /// </summary>
+        [JsonProperty("veteran")]
+        public bool IsVeteran { get; private set; }
+        /// <summary>
+        /// Gets <see langword="true"/> if League of Legends summoner is a new player
+        /// </summary>
+        [JsonProperty("freshBlood")]
+        public bool IsFreshBlood { get; private set; }
+        /// <summary>
+        /// Gets <see langword="true"/> if League of Legends summoner is inactive (was not logged in for too long)
+        /// </summary>
+        [JsonProperty("inactive")]
+        public bool IsInactive { get; private set; }
     }
 }
