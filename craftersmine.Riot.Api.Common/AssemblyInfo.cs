2cec0d871391f92b030af6b4fcd5a23a2836f0a4
craftersmine <rodion.morozov.01@gmail.com>
2022-10-14 13:31:22 +0700

    Updated project files and versions

--- craftersmine.Riot.Api.Common/AssemblyInfo.cs
+++ craftersmine.Riot.Api.Common/AssemblyInfo.cs
@@ -5,3 +5,4 @@ using System.Text;
 
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.Tests")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.Account")]
+[assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Summoner")]
