656024d40551c9970c57e39532416c901555fd6c
craftersmine <rodion.morozov.01@gmail.com>
2022-10-16 18:50:16 +0700

    Added endpoint root constant and XML docs

--- craftersmine.Riot.Api.League.Mastery/RiotLeagueMasteryApiClient.cs
+++ craftersmine.Riot.Api.League.Mastery/RiotLeagueMasteryApiClient.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using craftersmine.Riot.Api.Common;
 
 namespace craftersmine.Riot.Api.League.Mastery
 {
-    public class RiotLeagueMasteryApiClient
+    /// <summary>
+    /// Represents a Riot Champion-Mastery v4 API
+    /// </summary>
+    public class RiotLeagueMasteryApiClient : RiotApiClient
     {
+        private const string ApiEndpointRoot = "/lol/champion-mastery/v4/";
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RiotLeagueMasteryApiClient"/> instance
+        /// </summary>
+        /// <param name="settings">Settings for <see cref="RiotLeagueMasteryApiClient"/></param>
+        public RiotLeagueMasteryApiClient(RiotApiClientSettings settings) : base(settings) { }
+
 
     }
 }
