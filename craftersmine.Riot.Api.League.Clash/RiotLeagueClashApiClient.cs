eed3b18b7f204317fadda9f6d0de666d2f427002
craftersmine <rodion.morozov.01@gmail.com>
2022-10-16 22:10:23 +0700

    Added Clash v1 API project

---
+++ craftersmine.Riot.Api.League.Clash/RiotLeagueClashApiClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using craftersmine.Riot.Api.Common;
+
+namespace craftersmine.Riot.Api.League.Clash
+{
+    /// <summary>
+    /// Represents Riot League of Legends Clash v1 API
+    /// </summary>
+    public class RiotLeagueClashApiClient : RiotApiClient
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="RiotLeagueClashApiClient"/> instance
+        /// </summary>
+        /// <param name="settings">Settings for <see cref="RiotLeagueClashApiClient"/></param>
+        public RiotLeagueClashApiClient(RiotApiClientSettings settings) : base(settings) { }
+
+
+    }
+}
