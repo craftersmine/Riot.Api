669f0fc4feb576ebccf899b333cb8fa87926aa51
craftersmine <rodion.morozov.01@gmail.com>
2022-10-16 18:27:38 +0700

    Added Champion v3 API

---
+++ craftersmine.Riot.Api.Tests/ChampionRotations/ChampionRotationsApiClientTests.cs
@@ -0,0 +1,44 @@
+﻿using craftersmine.Riot.Api.Common;
+using craftersmine.Riot.Api.League.Summoner;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.League.ChampionRotations;
+
+namespace craftersmine.Riot.Api.Tests.ChampionRotations
+{
+    [TestClass]
+    public class ChampionRotationsApiClientTests
+    {
+        public const int FirstChampionId = 2;
+        public const int FirstChampionIdNewPlayers = 222;
+        public const int MaxNewPlayerLevel = 10;
+
+        public RiotChampionRotationsApiClient? Client { get; set; }
+        public string? ApiKey { get; set; }
+
+        public TestContext? TestContext { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ApiKey = TestContext?.Properties["ApiKey"]?.ToString();
+            if (string.IsNullOrWhiteSpace(ApiKey))
+                Assert.Fail("No Riot API key provided!");
+            Client = new RiotChampionRotationsApiClient(new RiotApiClientSettingsBuilder().UseApiKey(ApiKey).Build());
+        }
+
+        [TestMethod]
+        public async Task GetChampionRotationsTests()
+        {
+            Assert.IsNotNull(Client, "Client is not initialized");
+            ChampionRotationsInfo rotations = await Client.GetCurrentChampionRotationsForRegion(RiotPlatform.Russia);
+            Assert.IsTrue(rotations.FreeChampionIds.Contains(FirstChampionId), "Rotations doesn't have champion " + FirstChampionId + "in rotations");
+            Assert.IsTrue(rotations.FreeChampionIdsForNewPlayers.Contains(FirstChampionIdNewPlayers), "Rotations doesn't have champion " + FirstChampionIdNewPlayers + "in rotations for new players");
+            Assert.AreEqual(MaxNewPlayerLevel, rotations.MaxNewPlayerLevel, "Max new player level are not equal to " + MaxNewPlayerLevel);
+        }
+    }
+}
