a83af49c6f5753c814815fa9ca7077c7eac5e1d2
craftersmine <rodion.morozov.01@gmail.com>
2022-10-17 23:32:47 +0700

    Implemented Unix Time Span converter

---
+++ craftersmine.Riot.Api.Common/Converters/UnixTimeSpanConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Common.Converters
+{
+    internal class UnixTimeSpanConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is TimeSpan ts)
+            {
+                writer.WriteValue((long)ts.TotalMilliseconds);
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+                return new TimeSpan(0, 0, 0, 0, 0);
+
+            if (reader.Value != null)
+            {
+                if (reader.TokenType != JsonToken.Integer)
+                    throw new Exception("Expected integer value, got " + reader.TokenType);
+                long msTs = (long) reader.Value;
+                return TimeSpan.FromMilliseconds(msTs);
+            }
+
+            return new TimeSpan(0, 0, 0, 0, 0);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
