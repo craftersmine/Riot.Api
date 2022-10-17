35322cf1eaa3d92318903729d27338bcd3a189b6
craftersmine <rodion.morozov.01@gmail.com>
2022-10-17 21:50:06 +0700

    Implemented leftover API methods

--- craftersmine.Riot.Api.League.Clash/RiotLeagueClashApiClient.cs
+++ craftersmine.Riot.Api.League.Clash/RiotLeagueClashApiClient.cs
@@ -56,5 +56,59 @@ namespace craftersmine.Riot.Api.League.Clash
             LeagueClashTeam team = await Client.Get<LeagueClashTeam>(endpoint, null);
             return team;
         }
+
+        /// <summary>
+        /// Gets active and upcoming Clash Tournaments for specified region
+        /// </summary>
+        /// <param name="region">League of Legends server region</param>
+        /// <returns>An array of <see cref="LeagueClashTournament"/></returns>
+        /// <exception cref="craftersmine.Riot.Api.Common.Exceptions.RiotApiException">When Riot API request fails</exception>
+        public async Task<LeagueClashTournament[]> GetClashTournamentsAsync(RiotPlatform region)
+        {
+            string endpoint = UriUtils.GetAddress(region, UriUtils.JoinEndpoints(ApiEndpointRoot, "tournaments"));
+
+            LeagueClashTournament[] tournaments = await Client.Get<LeagueClashTournament[]>(endpoint, null);
+            return tournaments;
+        }
+
+        /// <summary>
+        /// Gets current Clash tournament for team with specified ID in specified region
+        /// </summary>
+        /// <param name="region">League of Legends server region</param>
+        /// <param name="teamId">Clash Team ID</param>
+        /// <returns><see cref="LeagueClashTournament"/> for specified team</returns>
+        /// <exception cref="ArgumentNullException">When team ID is null or empty</exception>
+        /// <exception cref="craftersmine.Riot.Api.Common.Exceptions.RiotApiException">When Riot API request fails</exception>
+        public async Task<LeagueClashTournament> GetClashTournamentByTeamIdAsync(RiotPlatform region, string teamId)
+        {
+            if (string.IsNullOrWhiteSpace(teamId))
+                throw new ArgumentNullException(nameof(teamId));
+
+            string endpoint = UriUtils.GetAddress(region,
+                UriUtils.JoinEndpoints(ApiEndpointRoot, "tournaments/by-team", teamId));
+
+            LeagueClashTournament tournament = await Client.Get<LeagueClashTournament>(endpoint, null);
+            return tournament;
+        }
+
+        /// <summary>
+        /// Gets a Clash Tournament by Tournament ID in specified region
+        /// </summary>
+        /// <param name="region">League of Legends server region</param>
+        /// <param name="tournamentId">Clash Tournament ID</param>
+        /// <returns><see cref="LeagueClashTournament"/> with specified tournament ID</returns>
+        /// <exception cref="ArgumentOutOfRangeException">When tournament ID is less than 0</exception>
+        /// <exception cref="craftersmine.Riot.Api.Common.Exceptions.RiotApiException">When Riot API request fails</exception>
+        public async Task<LeagueClashTournament> GetClashTournamentByIdAsync(RiotPlatform region, int tournamentId)
+        {
+            if (tournamentId < 0)
+                throw new ArgumentOutOfRangeException(nameof(tournamentId), "Tournament ID cannot be less than 0");
+
+            string endpoint = UriUtils.GetAddress(region,
+                UriUtils.JoinEndpoints(ApiEndpointRoot, "tournaments", tournamentId.ToString()));
+
+            LeagueClashTournament tournament = await Client.Get<LeagueClashTournament>(endpoint, null);
+            return tournament;
+        }
     }
 }
