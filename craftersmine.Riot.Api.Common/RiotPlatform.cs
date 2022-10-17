c99ee88502e4b4f55a797f195d5bad06c0dc960a
craftersmine <rodion.morozov.01@gmail.com>
2022-10-17 23:19:35 +0700

    Added extensions methods

--- craftersmine.Riot.Api.Common/RiotPlatform.cs
+++ craftersmine.Riot.Api.Common/RiotPlatform.cs
@@ -96,5 +96,71 @@ namespace craftersmine.Riot.Api.Common
                     throw new Exception("Something went wrong when selecting platform! This should not happen");
             }
         }
+
+        /// <summary>
+        /// Returns an corresponding string for <see cref="RiotPlatform"/>
+        /// </summary>
+        /// <param name="platform"><see cref="RiotPlatform"/></param>
+        /// <exception cref="ArgumentException">Should not happen</exception>
+        public static string GetRiotPlatformString(this RiotPlatform platform)
+        {
+            switch (platform)
+            {
+                case RiotPlatform.Brazil:
+                    return Constants.LolBrRegion;
+                case RiotPlatform.EuropeNordicEast:
+                    return Constants.LolEuneRegion;
+                case RiotPlatform.EuropeWest:
+                    return Constants.LolEuwRegion;
+                case RiotPlatform.LatinAmericaNorth:
+                    return Constants.LolLanRegion;
+                case RiotPlatform.LatinAmericaSouth:
+                    return Constants.LolLasRegion;
+                case RiotPlatform.NorthAmerica:
+                    return Constants.LolNaRegion;
+                case RiotPlatform.Oceania:
+                    return Constants.LolOceRegion;
+                case RiotPlatform.Russia:
+                    return Constants.LolRuRegion;
+                case RiotPlatform.Turkey:
+                    return Constants.LolTrRegion;
+                case RiotPlatform.Japan:
+                    return Constants.LolJpRegion;
+                case RiotPlatform.Korea:
+                    return Constants.LolKrRegion;
+            }
+            throw new ArgumentException("Unknown Riot Platform passed in!", nameof(platform));
+        }
+
+        internal static RiotPlatform GetRiotPlatformFromString(this string str)
+        {
+            switch (str)
+            {
+                case Constants.LolBrRegion:
+                    return RiotPlatform.Brazil;
+                case Constants.LolEuneRegion:
+                    return RiotPlatform.EuropeNordicEast;
+                case Constants.LolEuwRegion:
+                    return RiotPlatform.EuropeWest;
+                case Constants.LolLanRegion:
+                    return RiotPlatform.LatinAmericaNorth;
+                case Constants.LolLasRegion:
+                    return RiotPlatform.LatinAmericaSouth;
+                case Constants.LolNaRegion:
+                    return RiotPlatform.NorthAmerica;
+                case Constants.LolOceRegion:
+                    return RiotPlatform.Oceania;
+                case Constants.LolRuRegion:
+                    return RiotPlatform.Russia;
+                case Constants.LolTrRegion:
+                    return RiotPlatform.Turkey;
+                case Constants.LolJpRegion:
+                    return RiotPlatform.Japan;
+                case Constants.LolKrRegion:
+                    return RiotPlatform.Korea;
+                default:
+                    throw new ArgumentException("Unknown Riot Platform passed in!", nameof(str));
+            }
+        }
     }
 }
