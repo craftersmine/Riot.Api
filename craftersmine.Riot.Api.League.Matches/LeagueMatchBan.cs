8efa35d00cf2bc689b58be3f8989668f7ab6d771
craftersmine <rodion.morozov.01@gmail.com>
2022-10-18 01:39:41 +0700

    Added Match team and team ban objects

---
+++ craftersmine.Riot.Api.League.Matches/LeagueMatchBan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches
+{
+    /// <summary>
+    /// Represents a League of Legends match ban
+    /// </summary>
+    public class LeagueMatchBan
+    {
+        /// <summary>
+        /// Gets a League of Legends champion ID
+        /// </summary>
+        [JsonProperty("championId")]
+        public int ChampionId { get; private set; }
+        /// <summary>
+        /// Gets a ban pick turn
+        /// </summary>
+        [JsonProperty("pickTurn")]
+        public int PickTurn { get; private set; }
+    }
+}
---
+++ craftersmine.Riot.Api.League.Matches/LeagueMatchTeam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches
+{
+    /// <summary>
+    /// Represents a League of Legends match team
+    /// </summary>
+    public class LeagueMatchTeam
+    {
+        /// <summary>
+        /// Gets a League of Legends match team ID
+        /// </summary>
+        [JsonProperty("teamId")]
+        public int TeamId { get; private set; }
+        /// <summary>
+        /// Gets <see langword="true"/> if team is won this match
+        /// </summary>
+        [JsonProperty("win")]
+        public bool IsWon { get; private set; }
+        /// <summary>
+        /// Gets a League of Legends objectives information for this team
+        /// </summary>
+        [JsonProperty("objectives")]
+        public LeagueObjectives Objectives { get; private set; }
+        /// <summary>
+        /// Gets a set of League of Legends match bans for this team
+        /// </summary>
+        [JsonProperty("bans")]
+        public LeagueMatchBan[] Bans { get; private set; }
+    }
+}
