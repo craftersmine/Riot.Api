1e7f5435c6ecb9046fa5b9e6d7f873d01c052e99
craftersmine <rodion.morozov.01@gmail.com>
2022-10-20 03:08:31 +0700

    Added ILeagueTimelineEvent

---
+++ craftersmine.Riot.Api.League.Matches/Timeline/ILeagueTimelineFrameEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using craftersmine.Riot.Api.Common.Converters;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches.Timeline
+{
+    /// <summary>
+    /// Represents a generic League of Legends match timeline frame event
+    /// </summary>
+    public interface ILeagueTimelineFrameEvent
+    {
+        /// <summary>
+        /// Gets a League of Legends match timeline frame event type
+        /// </summary>
+        [JsonProperty("type"), JsonConverter(typeof(LeagueTimelineFrameEventType))]
+        LeagueTimelineFrameEventType Type { get; }
+        /// <summary>
+        /// Gets a League of Legends match timeline frame event timestamp from game start
+        /// </summary>
+        [JsonProperty("timestamp"), JsonConverter(typeof(UnixTimeSpanConverter), true)]
+        TimeSpan Timestamp { get; }
+    }
+}
