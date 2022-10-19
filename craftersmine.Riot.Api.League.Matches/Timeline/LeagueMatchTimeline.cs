869d3077b4b8e5b5a72204b5e84309d87bf682c5
craftersmine <rodion.morozov.01@gmail.com>
2022-10-20 03:08:19 +0700

    Added League match timeline

---
+++ craftersmine.Riot.Api.League.Matches/Timeline/LeagueMatchTimeline.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches.Timeline
+{
+    /// <summary>
+    /// Represents a League of Legends match timeline
+    /// </summary>
+    public class LeagueMatchTimeline
+    {
+        /// <inheritdoc cref="LeagueMatch.Metadata"/>
+        [JsonProperty("metadata")]
+        public LeagueMatchMetadata Metadata { get; private set; }
+        /// <summary>
+        /// Gets a League of Legends match timeline data
+        /// </summary>
+        [JsonProperty("info")]
+        public LeagueMatchTimelineInfo Timeline { get; private set; }
+    }
+}
