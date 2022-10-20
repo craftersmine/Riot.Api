0fbd0b684455599b162621ec13040e2a8931de78
craftersmine <rodion.morozov.01@gmail.com>
2022-10-20 23:16:08 +0700

    Updated interface for ILeagueTimelineFrameEvent

--- craftersmine.Riot.Api.League.Matches/Timeline/FrameEvents/ILeagueTimelineFrameEvent.cs
+++ craftersmine.Riot.Api.League.Matches/Timeline/FrameEvents/ILeagueTimelineFrameEvent.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using craftersmine.Riot.Api.Common.Converters;
+﻿using craftersmine.Riot.Api.Common.Converters;
+using craftersmine.Riot.Api.League.Matches.Converters;
 using Newtonsoft.Json;
 
-namespace craftersmine.Riot.Api.League.Matches.Timeline
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Matches.Timeline.FrameEvents
 {
     /// <summary>
-    /// Represents a generic League of Legends match timeline frame event
+    /// Represents any of League of Legends match timeline frame event
     /// </summary>
     public interface ILeagueTimelineFrameEvent
     {
         /// <summary>
         /// Gets a League of Legends match timeline frame event type
         /// </summary>
-        [JsonProperty("type"), JsonConverter(typeof(LeagueTimelineFrameEventType))]
         LeagueTimelineFrameEventType Type { get; }
         /// <summary>
         /// Gets a League of Legends match timeline frame event timestamp from game start
         /// </summary>
-        [JsonProperty("timestamp"), JsonConverter(typeof(UnixTimeSpanConverter), true)]
         TimeSpan Timestamp { get; }
     }
 }
