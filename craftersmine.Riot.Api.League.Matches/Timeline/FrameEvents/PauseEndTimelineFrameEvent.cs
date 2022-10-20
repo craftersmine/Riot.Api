a0f2f89ae6635fca1798503038c84ccd93d92488
craftersmine <rodion.morozov.01@gmail.com>
2022-10-20 23:17:54 +0700

    Added PauseEnd timeline frame event

---
+++ craftersmine.Riot.Api.League.Matches/Timeline/FrameEvents/PauseEndTimelineFrameEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using craftersmine.Riot.Api.Common.Converters;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches.Timeline.FrameEvents
+{
+    /// <summary>
+    /// Represents a League of Legends match timeline pause ended frame event
+    /// </summary>
+    public class PauseEndTimelineFrameEvent : BaseTimelineFrameEvent
+    {
+        /// <summary>
+        /// Gets a <see cref="DateTime"/> when <see cref="LeagueTimelineFrameEventType.PauseEnd"/> event was created
+        /// </summary>
+        [JsonProperty("realTimestamp"), JsonConverter(typeof(UnixDateTimeConverter), false)]
+        public DateTime RealTimestamp { get; private set; }
+    }
+}
