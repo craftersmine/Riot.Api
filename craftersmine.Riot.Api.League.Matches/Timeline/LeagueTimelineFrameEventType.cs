87e90952795e5eddca3d00f028310ae1471fde07
craftersmine <rodion.morozov.01@gmail.com>
2022-10-20 23:16:45 +0700

    Updated event order and added extension methods

--- craftersmine.Riot.Api.League.Matches/Timeline/LeagueTimelineFrameEventType.cs
+++ craftersmine.Riot.Api.League.Matches/Timeline/LeagueTimelineFrameEventType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace craftersmine.Riot.Api.League.Matches.Timeline
@@ -12,104 +13,209 @@ namespace craftersmine.Riot.Api.League.Matches.Timeline
         /// <summary>
         /// The game has been resumed
         /// </summary>
-        ///
-        /// PAUSE_END
         PauseEnd,
         /// <summary>
         /// Champion skill leveled up
         /// </summary>
-        ///
-        /// SKILL_LEVEL_UP
         SkillLevelUp,
         /// <summary>
         /// Item was purchased in item shop or crafted by Ornn
         /// </summary>
-        ///
-        /// ITEM_PURCHASED
         ItemPurchased,
-        /// <summary>
-        /// Ward of any type was placed
-        /// </summary>
-        ///
-        /// WARD_PLACED
-        WardPlaced,
-        /// <summary>
-        /// Champion leveled up
-        /// </summary>
-        ///
-        /// LEVEL_UP
-        LevelUp,
         /// <summary>
         /// Item was used or ward was placed
         /// </summary>
-        ///
-        /// ITEM_DESTROYED
         ItemDestroyed,
-        /// <summary>
-        /// Champion was killed
-        /// </summary>
-        ///
-        /// CHAMPION_KILL
-        ChampionKill,
-        /// <summary>
-        /// Special event happened when champion was killed (first blood, multi kill, etc.)
-        /// </summary>
-        ///
-        /// CHAMPION_SPECIAL_KILL
-        ChampionSpecialKill,
         /// <summary>
         /// Item purchase undo
         /// </summary>
-        ///
-        /// ITEM_UNDO
         ItemUndo,
         /// <summary>
         /// Item was sold
         /// </summary>
-        ///
-        /// ITEM_SOLD
         ItemSold,
+        /// <summary>
+        /// Ward of any type was placed
+        /// </summary>
+        WardPlaced,
+        /// <summary>
+        /// Ward was destroyed (stealth, fairlight, control, etc.)
+        /// </summary>
+        WardKill,
+        /// <summary>
+        /// Champion leveled up
+        /// </summary>
+        LevelUp,
+        /// <summary>
+        /// Champion was killed
+        /// </summary>
+        ChampionKill,
+        /// <summary>
+        /// Special event happened when champion was killed (first blood, multi kill, etc.)
+        /// </summary>
+        ChampionSpecialKill,
         /// <summary>
         /// Elite monster were killed (dragon, rift herald, Baron Nashor, etc.)
         /// </summary>
-        ///
-        /// ELITE_MONSTER_KILL
         EliteMonsterKill,
         /// <summary>
         /// Turret plate was destroyed
         /// </summary>
-        ///
-        /// TURRET_PLATE_DESTROYED
         TurretPlateDestroyed,
-        /// <summary>
-        /// Ward was destroyed (stealth, fairlight, control, etc.)
-        /// </summary>
-        ///
-        /// WARD_KILL
-        WardKill,
         /// <summary>
         /// Building was destroyed (turret, inhibitor, nexus, etc)
         /// </summary>
-        ///
-        /// BUILDING_KILL
         BuildingKill,
         /// <summary>
         /// Objective bounty starting
         /// </summary>
-        /// 
-        /// OBJECTIVE_BOUNTY_PRESTART
         ObjectiveBountyPrestart,
+        /// <summary>
+        /// Objective bounty ending
+        /// </summary>
+        [Obsolete("Assumption. For now it is unknown if this value even exists")]
+        ObjectiveBountyPreend,
         /// <summary>
         /// Dragon soul was obtained
         /// </summary>
-        ///
-        /// DRAGON_SOUL_GIVEN
         DragonSoulGiven,
         /// <summary>
         /// Game ended
         /// </summary>
-        ///
-        /// GAME_END
-        GameEnd
+        GameEnd,
+        /// <summary>
+        /// Champion is transformed (Kayn transformation)
+        /// </summary>
+        ChampionTransform,
+        /// <summary>
+        /// Unknown event type
+        /// </summary>
+        Unknown
+    }
+
+    /// <summary>
+    /// Contains static extensions for <see cref="LeagueTimelineFrameEventType"/>
+    /// </summary>
+    public static class LeagueTimelineEventTypeExtensions
+    {
+        private const string PauseEnd = "PAUSE_END";
+        private const string SkillLevelUp = "SKILL_LEVEL_UP";
+        private const string ItemPurchased = "ITEM_PURCHASED";
+        private const string ItemDestroyed = "ITEM_DESTROYED";
+        private const string ItemUndo = "ITEM_UNDO";
+        private const string ItemSold = "ITEM_SOLD";
+        private const string WardPlaced = "WARD_PLACED";
+        private const string WardKill = "WARD_KILL";
+        private const string LevelUp = "LEVEL_UP";
+        private const string ChampionKill = "CHAMPION_KILL";
+        private const string ChampionSpecialKill = "CHAMPION_SPECIAL_KILL";
+        private const string EliteMonsterKill = "ELITE_MONSTER_KILL";
+        private const string TurretPlateDestroyed = "TURRET_PLATE_DESTROYED";
+        private const string BuildingKill = "BUILDING_KILL";
+        private const string ObjectiveBountyPrestart = "OBJECTIVE_BOUNTY_PRESTART";
+        private const string ObjectiveBountyPreend = "OBJECTIVE_BOUNTY_PREEND";
+        private const string DragonSoulGiven = "DRAGON_SOUL_GIVEN";
+        private const string GameEnd = "GAME_END";
+        private const string ChampionTranform = "CHAMPION_TRANSFORM";
+
+        /// <summary>
+        /// Gets a corresponding string for <see cref="LeagueTimelineFrameEventType"/>
+        /// </summary>
+        /// <param name="eventType">League of Legends match timeline frame event type value</param>
+        /// <returns>A corresponding <see langword="string"/> for <see cref="LeagueTimelineFrameEventType"/> value</returns>
+        public static string GetStringForEventType(this LeagueTimelineFrameEventType eventType)
+        {
+            switch (eventType)
+            {
+                case LeagueTimelineFrameEventType.PauseEnd:
+                    return PauseEnd;
+                case LeagueTimelineFrameEventType.SkillLevelUp:
+                    return SkillLevelUp;
+                case LeagueTimelineFrameEventType.ItemPurchased:
+                    return ItemPurchased;
+                case LeagueTimelineFrameEventType.ItemDestroyed:
+                    return ItemDestroyed;
+                case LeagueTimelineFrameEventType.ItemUndo:
+                    return ItemUndo;
+                case LeagueTimelineFrameEventType.ItemSold:
+                    return ItemSold;
+                case LeagueTimelineFrameEventType.WardPlaced:
+                    return WardPlaced;
+                case LeagueTimelineFrameEventType.WardKill:
+                    return WardKill;
+                case LeagueTimelineFrameEventType.LevelUp:
+                    return LevelUp;
+                case LeagueTimelineFrameEventType.ChampionKill:
+                    return ChampionKill;
+                case LeagueTimelineFrameEventType.ChampionSpecialKill:
+                    return ChampionSpecialKill;
+                case LeagueTimelineFrameEventType.EliteMonsterKill:
+                    return EliteMonsterKill;
+                case LeagueTimelineFrameEventType.TurretPlateDestroyed:
+                    return TurretPlateDestroyed;
+                case LeagueTimelineFrameEventType.BuildingKill:
+                    return BuildingKill;
+                case LeagueTimelineFrameEventType.ObjectiveBountyPrestart:
+                    return ObjectiveBountyPrestart;
+                case LeagueTimelineFrameEventType.ObjectiveBountyPreend:
+                    return ObjectiveBountyPreend;
+                case LeagueTimelineFrameEventType.DragonSoulGiven:
+                    return DragonSoulGiven;
+                case LeagueTimelineFrameEventType.GameEnd:
+                    return GameEnd;
+                case LeagueTimelineFrameEventType.ChampionTransform:
+                    return ChampionTranform;
+                default:
+                    throw new Exception("Unknown event type enum value, shouldn't happen");
+            }
+        }
+
+        internal static LeagueTimelineFrameEventType GetLeagueTimelineFrameEventTypeFromString(this string str)
+        {
+            switch (str)
+            {
+                case PauseEnd:
+                    return LeagueTimelineFrameEventType.PauseEnd;
+                case SkillLevelUp:
+                    return LeagueTimelineFrameEventType.SkillLevelUp;
+                case ItemPurchased:
+                    return LeagueTimelineFrameEventType.ItemPurchased;
+                case ItemDestroyed:
+                    return LeagueTimelineFrameEventType.ItemDestroyed;
+                case ItemUndo:
+                    return LeagueTimelineFrameEventType.ItemUndo;
+                case ItemSold:
+                    return LeagueTimelineFrameEventType.ItemSold;
+                case WardPlaced:
+                    return LeagueTimelineFrameEventType.WardPlaced;
+                case WardKill:
+                    return LeagueTimelineFrameEventType.WardKill;
+                case LevelUp:
+                    return LeagueTimelineFrameEventType.LevelUp;
+                case ChampionKill:
+                    return LeagueTimelineFrameEventType.ChampionKill;
+                case ChampionSpecialKill:
+                    return LeagueTimelineFrameEventType.ChampionSpecialKill;
+                case EliteMonsterKill:
+                    return LeagueTimelineFrameEventType.EliteMonsterKill;
+                case TurretPlateDestroyed:
+                    return LeagueTimelineFrameEventType.TurretPlateDestroyed;
+                case BuildingKill:
+                    return LeagueTimelineFrameEventType.BuildingKill;
+                case ObjectiveBountyPrestart:
+                    return LeagueTimelineFrameEventType.ObjectiveBountyPrestart;
+                case ObjectiveBountyPreend:
+                    return LeagueTimelineFrameEventType.ObjectiveBountyPreend;
+                case DragonSoulGiven:
+                    return LeagueTimelineFrameEventType.DragonSoulGiven;
+                case GameEnd:
+                    return LeagueTimelineFrameEventType.GameEnd;
+                case ChampionTranform:
+                    return LeagueTimelineFrameEventType.ChampionTransform;
+                default:
+                    //throw new ArgumentException("Unknown match timeline frame event type received! Value: " + str);
+                    return LeagueTimelineFrameEventType.Unknown;
+            }
+        }
     }
 }
