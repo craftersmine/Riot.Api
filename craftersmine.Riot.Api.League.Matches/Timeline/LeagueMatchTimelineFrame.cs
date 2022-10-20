79eedb98e1c65e6eea45eee254a95c3dcf5259c2
craftersmine <rodion.morozov.01@gmail.com>
2022-10-20 23:18:55 +0700

    Implemented basic conversion of event objects

--- craftersmine.Riot.Api.League.Matches/Timeline/LeagueMatchTimelineFrame.cs
+++ craftersmine.Riot.Api.League.Matches/Timeline/LeagueMatchTimelineFrame.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using craftersmine.Riot.Api.Common.Converters;
+using craftersmine.Riot.Api.League.Matches.Converters;
+using craftersmine.Riot.Api.League.Matches.Timeline.FrameEvents;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using UnixDateTimeConverter = craftersmine.Riot.Api.Common.Converters.UnixDateTimeConverter;
 
 namespace craftersmine.Riot.Api.League.Matches.Timeline
 {
@@ -11,20 +17,57 @@ namespace craftersmine.Riot.Api.League.Matches.Timeline
     /// </summary>
     public class LeagueMatchTimelineFrame
     {
+        private JsonSerializer serializer;
+
+        [JsonProperty("events")]
+        private JObject[] EventsRaw { get; set; }
+
         /// <summary>
         /// Gets an array of League of Legends match timeline current frame events
         /// </summary>
-        [JsonProperty("events")]
-        public ILeagueMatchTimelineFrameEvent[] Events { get; private set; }
+        public ILeagueTimelineFrameEvent[] Events => GetFromRawEvents();
         /// <summary>
         /// Gets an array of League of Legends match timeline current participant frames
         /// </summary>
         [JsonProperty("participantFrames")]
-        public LeagueMatchTimelineParticipantFrame[] ParticipantFrames { get; private set; }
+        public LeagueMatchTimelineParticipantFrameCollection ParticipantFrames { get; private set; }
         /// <summary>
         /// Gets a <see cref="TimeSpan"/> timestamp of frame from the game beginning
         /// </summary>
         [JsonProperty("timestamp"), JsonConverter(typeof(UnixTimeSpanConverter), true)]
         public TimeSpan Timestamp { get; private set; }
+
+        internal ILeagueTimelineFrameEvent[] GetFromRawEvents()
+        {
+            if (serializer is null)
+            {
+                serializer = JsonSerializer.CreateDefault();
+                
+                serializer.Converters.Add(new LeagueTimelineFrameEventTypeConverter());
+                serializer.Converters.Add(new UnixDateTimeConverter(true));
+            }
+
+            List<ILeagueTimelineFrameEvent> _events = new List<ILeagueTimelineFrameEvent>();
+            foreach (var e in EventsRaw)
+            {
+                BaseTimelineFrameEvent evt = DeserializeJObjectTo<BaseTimelineFrameEvent>(e);
+                switch (evt.Type)
+                {
+                    case LeagueTimelineFrameEventType.PauseEnd:
+                        _events.Add(DeserializeJObjectTo<PauseEndTimelineFrameEvent>(e));
+                        break;
+                    default:
+                        _events.Add(evt);
+                        break;
+                }
+            }
+
+            return _events.ToArray();
+        }
+
+        private T DeserializeJObjectTo<T>(JObject jObject)
+        {
+            return JsonConvert.DeserializeObject<T>(jObject.ToString());
+        }
     }
 }
