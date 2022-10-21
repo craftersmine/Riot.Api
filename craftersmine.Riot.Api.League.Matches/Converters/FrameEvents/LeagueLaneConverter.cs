155cb02b8fccd8744085d4003db3ee4d91dd60d9
craftersmine <rodion.morozov.01@gmail.com>
2022-10-21 20:49:55 +0700

    Added LeagueLane converter

---
+++ craftersmine.Riot.Api.League.Matches/Converters/FrameEvents/LeagueLaneConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using craftersmine.Riot.Api.League.Matches.Timeline.FrameEvents;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches.Converters.FrameEvents
+{
+    internal class LeagueLaneConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is LeagueLane lane)
+                writer.WriteValue(lane.GetStringFor());
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                if (reader.Value != null)
+                    reader.Value.ToString().GetLeagueLaneFromString();
+            throw new JsonReaderException("Unable to convert value to LeagueLane: " + reader.Value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(LeagueLane);
+        }
+    }
+}
