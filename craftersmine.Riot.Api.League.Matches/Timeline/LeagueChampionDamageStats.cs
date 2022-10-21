355aee30ce4328bc963fabd12a5a32174ef9e169
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 02:10:12 +0700

    Added skeleton for League champion damage stats class

---
+++ craftersmine.Riot.Api.League.Matches/Timeline/LeagueChampionDamageStats.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Matches.Timeline
+{
+    public class LeagueChampionDamageStats
+    {
+        // TODO (craftersmine): implement champion damage stats class
+    }
+}
