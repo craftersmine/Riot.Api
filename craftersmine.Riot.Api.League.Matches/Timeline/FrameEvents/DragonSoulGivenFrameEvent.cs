2c4559a2e881f6e17352b06901f706fab1a6848b
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 01:25:00 +0700

    Added dragon soul given frame event

---
+++ craftersmine.Riot.Api.League.Matches/Timeline/FrameEvents/DragonSoulGivenFrameEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches.Timeline.FrameEvents
+{
+    /// <summary>
+    /// Represents a League of Legends dragon soul being captured timeline frame event
+    /// </summary>
+    public class DragonSoulGivenFrameEvent : BaseTimelineFrameEvent
+    {
+        /// <summary>
+        /// Gets a team ID which captured dragon soul in the match
+        /// </summary>
+        [JsonProperty("teamId")]
+        public int TeamId { get; private set; }
+        /// <summary>
+        /// Gets a name of dragon soul that was captured
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+        // TODO (craftersmine): add enum for dragon soul name?
+    }
+}
