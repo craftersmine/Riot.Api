ced284e1bd9e4556fe515c68f11a2a669eaaa87d
craftersmine <rodion.morozov.01@gmail.com>
2022-10-21 19:45:13 +0700

    Changed string game ID

--- craftersmine.Riot.Api.Tests/League/MatchesApiClientTests.cs
+++ craftersmine.Riot.Api.Tests/League/MatchesApiClientTests.cs
@@ -16,7 +16,7 @@ namespace craftersmine.Riot.Api.Tests.League
     public class MatchesApiClientTests
     {
         public const string MyPuuid = "XRWOl9NePvqBbDsCBQKMgks13Cyc5KO1N-ZCPE0xr8Yvt58H7JjiWx_jlkF4VSYc31kBYfoKZtSNhA";
-        public const string GameId = "RU_414155060";
+        public const string GameId = "RU_413971566";
         public const string GameMode = "CLASSIC";
         public const long GameIdLong = 413971566;
         public const int QueueId = 420; // Ranked Solo Queue Summoner Rift
