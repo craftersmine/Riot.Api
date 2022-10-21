303232afb8c370442dd3da5388710793a5ed384a
craftersmine <rodion.morozov.01@gmail.com>
2022-10-21 19:46:28 +0700

    Added League special kill type

---
+++ craftersmine.Riot.Api.League.Matches/Converters/FrameEvents/LeagueSpecialKillTypeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using craftersmine.Riot.Api.League.Matches.Timeline.FrameEvents;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Matches.Converters.FrameEvents
+{
+    internal class LeagueSpecialKillTypeConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is LeagueSpecialKillType specialKillType)
+            {
+                writer.WriteValue(specialKillType.GetStringFor());
+            }
+
+            throw new ArgumentException("Unknown value is passed as special kill type " + value.GetType().Name, nameof(value));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+            {
+                if (reader.Value != null) return reader.Value.ToString().GetLeagueSpecialKillTypeFromString();
+            }
+
+            throw new JsonReaderException("Unknown token for special kill value " + reader.Value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(LeagueSpecialKillType);
+        }
+    }
+}
