4ec9a89ecfe74e65c2fd40c2966b8a99dc815d23
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 22:10:19 +0700

    Added Status content

---
+++ craftersmine.Riot.Api.League.Status/RiotServiceStatusContent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Status
+{
+    /// <summary>
+    /// Represents a localized Riot Service status message
+    /// </summary>
+    public class RiotServiceStatusContent
+    {
+        /// <summary>
+        /// Gets a localized content for Riot Service status message
+        /// </summary>
+        [JsonProperty("content")]
+        public string Content { get; private set; }
+        /// <summary>
+        /// Gets a corresponding culture info for Riot Service status message locale
+        /// </summary>
+        [JsonProperty("locale"), JsonConverter(typeof(Common.Converters.CultureInfoConverter))]
+        public CultureInfo Locale { get; private set; }
+    }
+}
