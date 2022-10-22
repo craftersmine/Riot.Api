441ca0ee26d12639cbd0eb296d0a443d48389b31
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 02:31:30 +0700

    Added XML docs

--- craftersmine.Riot.Api.League.Status/RiotServiceStatusApiClient.cs
+++ craftersmine.Riot.Api.League.Status/RiotServiceStatusApiClient.cs
@@ -5,11 +5,23 @@ using craftersmine.Riot.Api.Common.Utils;
 
 namespace craftersmine.Riot.Api.Status
 {
+    /// <summary>
+    /// Represents a Riot Services Status API client
+    /// </summary>
     public class RiotServiceStatusApiClient : RiotApiClient
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="RiotServiceStatusApiClient"/> with specified settings
+        /// </summary>
+        /// <param name="settings"></param>
         public RiotServiceStatusApiClient(RiotApiClientSettings settings) : base(settings)
         { }
 
+        /// <summary>
+        /// Gets a Status for League of Legends server
+        /// </summary>
+        /// <param name="serverRegion">League of Legends server location</param>
+        /// <returns>A status for specified League of Legends server</returns>
         public async Task<RiotServiceStatus> GetLeagueStatusForRegionAsync(RiotPlatform serverRegion)
         {
             string endpoint =
@@ -18,7 +30,12 @@ namespace craftersmine.Riot.Api.Status
             RiotServiceStatus serviceStatus = await Client.Get<RiotServiceStatus>(endpoint, null);
             return serviceStatus;
         }
-
+        
+        /// <summary>
+        /// Gets a Status for Valorant server
+        /// </summary>
+        /// <param name="serverRegion">Valorant server location</param>
+        /// <returns>A status for specified Valorant server</returns>
         public async Task<RiotServiceStatus> GetValorantStatusForRegionAsync(RiotShards serverRegion)
         {
             if (serverRegion == RiotShards.Unknown || serverRegion == RiotShards.LoREurope ||
@@ -32,7 +49,12 @@ namespace craftersmine.Riot.Api.Status
             RiotServiceStatus serviceStatus = await Client.Get<RiotServiceStatus>(endpoint, null);
             return serviceStatus;
         }
-
+        
+        /// <summary>
+        /// Gets a Status for Legends of Runeterra server
+        /// </summary>
+        /// <param name="serverRegion">Legends of Runeterra server location</param>
+        /// <returns>A status for specified Legends of Runeterra server</returns>
         public async Task<RiotServiceStatus> GetLoRStatusForRegionAsync(RiotShards serverRegion)
         {
             if (serverRegion == RiotShards.Unknown || serverRegion == RiotShards.ValorantAsiaPacific ||
