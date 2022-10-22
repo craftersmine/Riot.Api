7abfd12b64c814b5c05998a06be7a6d655f322c8
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 23:37:19 +0700

    Added Service Platform converter

---
+++ craftersmine.Riot.Api.League.Status/Converters/RiotServicePlatformConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace craftersmine.Riot.Api.Status.Converters
+{
+    internal class RiotServicePlatformConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is RiotServicePlatform servicePlatform)
+                writer.WriteValue(servicePlatform.GetStringFor());
+
+            writer.WriteNull();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                if (reader.Value != null)
+                    return reader.Value.ToString().GetRiotServicePlatformFromString();
+            throw new JsonReaderException($"Unable to convert value {reader.Value} to {nameof(RiotServicePlatform)}");
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(RiotServicePlatform);
+        }
+    }
+}
