2c042e0cbc7ffbf888f734809503cf50aaca5009
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 20:18:43 +0700

    Fixed namespace

--- craftersmine.Riot.Api.Tests/League/StatusApiClientTests.cs
+++ craftersmine.Riot.Api.Tests/League/StatusApiClientTests.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using craftersmine.Riot.Api.League.Status;
+using craftersmine.Riot.Api.Status;
 
 namespace craftersmine.Riot.Api.Tests.League
 {
