ff62c446044c9af02b8daf1af1a483fece3f02aa
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 20:17:32 +0700

    Added Riot Service incident severity enum converter

---
+++ craftersmine.Riot.Api.League.Status/Converters/RiotServiceIncidentSeverityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Status.Converters
+{
+    internal class RiotServiceIncidentSeverityConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is RiotServiceIncidentSeverity incidentSeverity)
+                writer.WriteValue(incidentSeverity);
+
+            writer.WriteNull();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                if (reader.Value != null)
+                    return reader.Value.ToString().GetRiotServiceIncidentSeverityFromString();
+            return RiotServiceIncidentSeverity.Unknown;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(RiotServiceIncidentSeverity);
+        }
+    }
+}
