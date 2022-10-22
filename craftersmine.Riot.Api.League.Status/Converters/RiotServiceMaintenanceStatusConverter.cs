a32cf827f098a6dc436d91b1d9a86e1173d6ae08
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 20:17:05 +0700

    Added Riot Service maintenance status enum converter

---
+++ craftersmine.Riot.Api.League.Status/Converters/RiotServiceMaintenanceStatusConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Status.Converters
+{
+    internal class RiotServiceMaintenanceStatusConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is RiotServiceMaintenanceStatus maintenanceStatus)
+                writer.WriteValue(maintenanceStatus.GetStringFor());
+
+            writer.WriteNull();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                if (reader.Value != null)
+                    return reader.Value.ToString().GetRiotServiceMaintenanceStatusFromString();
+            return RiotServiceMaintenanceStatus.Unknown;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(RiotServiceMaintenanceStatus);
+        }
+    }
+}
