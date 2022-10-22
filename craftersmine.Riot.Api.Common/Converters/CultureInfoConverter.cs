fd235a336aede84109ab526a7940973810a8eed5
craftersmine <rodion.morozov.01@gmail.com>
2022-10-22 17:31:09 +0700

    Added CultureInfoConverter

---
+++ craftersmine.Riot.Api.Common/Converters/CultureInfoConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Common.Converters
+{
+    internal class CultureInfoConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is CultureInfo cultureInfo)
+                writer.WriteValue(cultureInfo.Name);
+
+            throw new ArgumentException($"Unable to convert type \"{value.GetType().Name}\", expected " +
+                                        nameof(CultureInfo));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                if (reader.Value != null)
+                    return new CultureInfo(reader.Value.ToString());
+
+            return CultureInfo.InvariantCulture;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(CultureInfo);
+        }
+    }
+}
