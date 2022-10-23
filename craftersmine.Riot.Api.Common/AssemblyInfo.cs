9fc27ebb64ac99f7cdb6a5434b537e56f1651a5a
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 13:20:54 +0700

    Added League Challenges API project

--- craftersmine.Riot.Api.Common/AssemblyInfo.cs
+++ craftersmine.Riot.Api.Common/AssemblyInfo.cs
@@ -12,4 +12,5 @@ using System.Text;
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Clash")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Matches")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Matches.Timeline")]
+[assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Challenges")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.Status")]
---
+++ craftersmine.Riot.Api.League.Challenges/RiotLeagueChallengesApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using craftersmine.Riot.Api.Common;
+
+namespace craftersmine.Riot.Api.League.Challenges
+{
+    public class RiotLeagueChallengesApiClient : RiotApiClient
+    {
+
+    }
+}
