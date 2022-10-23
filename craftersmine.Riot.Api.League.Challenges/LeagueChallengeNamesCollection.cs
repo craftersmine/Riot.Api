f02661907ee9d6460cb0c330952ee484e8146bf3
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 14:23:58 +0700

    Implemented string and CultureInfo indexers

--- craftersmine.Riot.Api.League.Challenges/LeagueChallengeNamesCollection.cs
+++ craftersmine.Riot.Api.League.Challenges/LeagueChallengeNamesCollection.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 using craftersmine.Riot.Api.Common.Converters;
 using Newtonsoft.Json;
@@ -12,6 +13,38 @@ namespace craftersmine.Riot.Api.League.Challenges
     /// </summary>
     public class LeagueChallengeNamesCollection : Dictionary<string, LeagueChallengeName>
     {
+        /// <summary>
+        /// Gets a League of Legends challenge localized name and descriptions with specified Riot Locale ID or languagecode2-country code
+        /// </summary>
+        /// <param name="localeId">Riot Formatted locale ID of languagecode2-country code</param>
+        /// <returns><see cref="LeagueChallengeName"/> with specified locale ID or <see langword="null"/> if nothing found for specified locale ID</returns>
+        public new LeagueChallengeName this[string localeId]
+        {
+            get
             {
+                if (localeId.Contains('-'))
+                    localeId = localeId.Replace('-', '_');
+
+                return this.FirstOrDefault(c => c.Key == localeId).Value;
+            }
+
+            private set
+            {
+                this.Add(localeId.Replace('-', '_'), value);
+            }
+        }
+
+        /// <summary>
+        /// Gets a League of Legends localized name and descriptions by specified <see cref="CultureInfo"/> data
+        /// </summary>
+        /// <param name="cultureInfo"><see cref="CultureInfo"/> to search for</param>
+        /// <returns><see cref="LeagueChallengeName"/> with specified locale ID or <see langword="null"/> if nothing found for specified <see cref="CultureInfo"/></returns>
+        public LeagueChallengeName this[CultureInfo cultureInfo]
+        {
+            get
+            {
+                return this.FirstOrDefault(c => c.Key.Replace('_', '-') == cultureInfo.Name).Value;
+            }
+        }
     }
 }
