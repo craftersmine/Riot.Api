49b291d7d13babe8465cff98d36cafeac4a1753c
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 14:22:26 +0700

    Added League challenge collection

---
+++ craftersmine.Riot.Api.League.Challenges/LeagueChallengeCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Challenges
+{
+    /// <summary>
+    /// Represents a League of Legends challenges collection
+    /// </summary>
+    public class LeagueChallengeCollection : List<LeagueChallenge>
+    {
+        /// <summary>
+        /// Gets a League of Legends challenge by specified ID
+        /// </summary>
+        /// <param name="challengeId">League of Legends challenge ID</param>
+        /// <returns><see cref="LeagueChallenge"/> with specified ID or <see langword="null"/></returns>
+        public LeagueChallenge GetChallengeById(int challengeId)
+        {
+            return this.FirstOrDefault(c => c.Id == challengeId);
+        }
+    }
+}
