1cd97fc5dac6438548e032bf88baa6b3cabbaaee
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 13:57:35 +0700

    Added League challenge names collection class

---
+++ craftersmine.Riot.Api.League.Challenges/LeagueChallengeNamesCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using craftersmine.Riot.Api.Common.Converters;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Challenges
+{
+    /// <summary>
+    /// Represents a collection of League of Legends challenge localized names and descriptions
+    /// </summary>
+    [JsonConverter(typeof(CultureInfoConverter))]
+    public class LeagueChallengeNamesCollection : Dictionary<CultureInfo, LeagueChallengeName>
+    {
+    }
+}
