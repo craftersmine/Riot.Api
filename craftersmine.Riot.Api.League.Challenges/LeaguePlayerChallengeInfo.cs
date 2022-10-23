d0e00436433e520a87a5b412f3d1c00c8e604d27
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 19:38:48 +0700

    Added League player challenge info

---
+++ craftersmine.Riot.Api.League.Challenges/LeaguePlayerChallengeInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using craftersmine.Riot.Api.Common.Converters;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Challenges
+{
+    /// <summary>
+    /// Represents a League of Legends challenge information for player
+    /// </summary>
+    public class LeaguePlayerChallengeInfo : LeagueChallengeBaseInfo
+    {
+        /// <summary>
+        /// Gets an ID of challenge for which these stats are set
+        /// </summary>
+        [JsonProperty("challengeId")]
+        public int ChallengeId { get; private set; }
+        /// <summary>
+        /// Gets a <see cref="DateTime"/> timestamp when player achieved current challenge level
+        /// </summary>
+        [JsonProperty("achievedTime"), JsonConverter(typeof(UnixDateTimeConverter), true)]
+        public DateTime AchievetAt { get; private set; }
+    }
+}
