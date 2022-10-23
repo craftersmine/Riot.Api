e2aa62b165ac918da8c1d8b79b1bc5b48e5801ef
craftersmine <rodion.morozov.01@gmail.com>
2022-10-24 00:09:18 +0700

    Inherited RiotApiClient

--- craftersmine.Riot.Api.League.Tournament/RiotLeagueTournamentApiClient.cs
+++ craftersmine.Riot.Api.League.Tournament/RiotLeagueTournamentApiClient.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using craftersmine.Riot.Api.Common;
 
 namespace craftersmine.Riot.Api.League.Tournament
 {
-    public class RiotLeagueTournamentApiClient
+    public class RiotLeagueTournamentApiClient : RiotApiClient
     {
+        private const string ApiEndpointRoot = "/lol/tournament/v4/";
+        private const string ApiStubEndpointRoot = "/lol/tournament-stub/v4/";
 
+        public RiotLeagueTournamentApiClient(RiotApiClientSettings settings) : base(settings) { }
+
+        
     }
 }
