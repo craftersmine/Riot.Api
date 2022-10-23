e61bf351a6804be96d89e4aaaba70238ecdeb1a4
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 14:48:39 +0700

    Added League challenge percentiles collection

---
+++ craftersmine.Riot.Api.League.Challenges/LeagueChallengePercentilesCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Challenges
+{
+    /// <summary>
+    /// Represents a collection of League of Legends challenges ranking percentiles
+    /// </summary>
+    public class LeagueChallengePercentilesCollection : Dictionary<int, LeagueChallengePercentiles>
+    {
+        /// <summary>
+        /// Gets a League of Legends challenge percentiles by specified challenge ID
+        /// </summary>
+        /// <param name="challengeId">League of Legends challenge ID</param>
+        /// <returns><see cref="LeagueChallengePercentiles"/> for specified challenge ID</returns>
+        public new LeagueChallengePercentiles this[int challengeId]
+        {
+            get => base[challengeId];
+
+            private set => base[challengeId] = value;
+        }
+    }
+}
