fe0cd00ec20759c66f684f9307204d865c3b29fe
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 13:57:55 +0700

    Added enum converters

---
+++ craftersmine.Riot.Api.League.Challenges/Converters/LeagueChallengeStateConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Challenges.Converters
+{
+    internal class LeagueChallengeStateConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is LeagueChallengeState challengeState)
+                writer.WriteValue(challengeState.GetStringFor());
+
+            writer.WriteNull();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                if (reader.Value != null)
+                    return reader.Value.ToString().GetLeagueChallengeStateFromString();
+            return LeagueChallengeState.Unknown;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(LeagueChallengeState);
+        }
+    }
+}
