a1e191bc857278d1a862e9d8087b63c51a1ed61e
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 14:21:53 +0700

    Implemented Get Challenges method

--- craftersmine.Riot.Api.League.Challenges/RiotLeagueChallengesApiClient.cs
+++ craftersmine.Riot.Api.League.Challenges/RiotLeagueChallengesApiClient.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Threading.Tasks;
 using craftersmine.Riot.Api.Common;
+using craftersmine.Riot.Api.Common.Utils;
 
 namespace craftersmine.Riot.Api.League.Challenges
 {
     public class RiotLeagueChallengesApiClient : RiotApiClient
     {
+        private const string ApiEndpointRoot = "/lol/challenges/v1/";
 
+        public RiotLeagueChallengesApiClient(RiotApiClientSettings settings) : base(settings) { }
+
+        public async Task<LeagueChallengeCollection> GetLeagueChallenges(RiotPlatform region)
+        {
+            string endpoint = UriUtils.GetAddress(region, UriUtils.JoinEndpoints(ApiEndpointRoot, "challenges/config"));
+
+            LeagueChallengeCollection leagueChallenges = await Client.Get<LeagueChallengeCollection>(endpoint, null);
+            return leagueChallenges;
+        }
     }
 }
