a0a84f79e5f1e99489755d0723e08bcea577f9e1
craftersmine <rodion.morozov.01@gmail.com>
2022-10-23 19:38:01 +0700

    Added League challenges player data

---
+++ craftersmine.Riot.Api.League.Challenges/LeagueChallengesPlayerData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Challenges
+{
+    /// <summary>
+    /// Represents a League of Legends challenges data for player
+    /// </summary>
+    public class LeagueChallengesPlayerData
+    {
+        /// <summary>
+        /// Gets a total information for challenges of player
+        /// </summary>
+        [JsonProperty("totalPoints")]
+        public LeagueChallengeBaseInfo TotalPoints { get; private set; }
+        /// <summary>
+        /// Gets an information about challenge categories of player
+        /// </summary>
+        [JsonProperty("categoryPoints")]
+        public LeagueChallengeCategoryInfo CategoryPoints { get; private set; }
+        /// <summary>
+        /// Gets a collection of challenge infos for player
+        /// </summary>
+        [JsonProperty("challenges")]
+        public LeaguePlayerChallengeInfosCollection Challenges { get; private set; }
+        /// <summary>
+        /// Gets a current League Client preferences specific to challenges
+        /// </summary>
+        [JsonProperty("preferences")]
+        public LeagueChallengesPlayerClientPreferences ClientPreferences { get; private set; }
+    }
+}
