6eeb15b264e405eff77d5cea291450f359301972
craftersmine <rodion.morozov.01@gmail.com>
2022-10-24 16:19:09 +0700

    Added Tournament API tests

---
+++ craftersmine.Riot.Api.Tests/League/TournamentApiTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.Riot.Api.Tests.League
+{
+    internal class TournamentApiTests
+    {
+    }
+}
