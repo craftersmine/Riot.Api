7facf6083e8567413b0e2ea0b21d877a69f156a7
craftersmine <rodion.morozov.01@gmail.com>
2022-10-24 15:10:22 +0700

    Added tournament lobby event

---
+++ craftersmine.Riot.Api.League.Tournament/LeagueTournamentEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using craftersmine.Riot.Api.Common.Converters;
+using craftersmine.Riot.Api.League.Tournament.Converters;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Tournament
+{
+    /// <summary>
+    /// Represents a League of Legends tournament event
+    /// </summary>
+    public class LeagueTournamentEvent
+    {
+        /// <summary>
+        /// Gets a <see cref="DateTime"/> timestamp when event occurred
+        /// </summary>
+        [JsonProperty("timestamp"), JsonConverter(typeof(UnixDateTimeConverter), true)]
+        public DateTime Timestamp { get; private set; }
+        /// <summary>
+        /// Gets a type of tournament event
+        /// </summary>
+        [JsonProperty("eventType"), JsonConverter(typeof(LeagueTournamentEventTypeConverter))]
+        public LeagueTournamentEventType EventType { get; private set; }
+        /// <summary>
+        /// Gets a summoner ID that caused this event
+        /// </summary>
+        [JsonProperty("summonerId")]
+        public string SummonerId { get; private set; }
+    }
+}
