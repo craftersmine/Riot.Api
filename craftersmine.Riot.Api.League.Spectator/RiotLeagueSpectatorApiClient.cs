d434bda006cdcdccd8c7074bec1d3ad42960f430
craftersmine <rodion.morozov.01@gmail.com>
2022-10-26 00:35:36 +0700

    Added Spectator v4 API project

---
+++ craftersmine.Riot.Api.League.Spectator/RiotLeagueSpectatorApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using craftersmine.Riot.Api.Common;
+
+namespace craftersmine.Riot.Api.League.Spectator
+{
+    public class RiotLeagueSpectatorApiClient : RiotApiClient
+    {
+        public RiotLeagueSpectatorApiClient(RiotApiClientSettings settings) : base(settings) { }
+    }
+}
