edde49c2446bee4a461415b1737e919009f7d327
craftersmine <rodion.morozov.01@gmail.com>
2022-10-26 01:38:10 +0700

    Added Spectator v4 API DTOs

---
+++ craftersmine.Riot.Api.League.Spectator/LeagueCurrentGameParticipant.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Spectator
+{
+    /// <summary>
+    /// Represents a participant in current League of Legends game
+    /// </summary>
+    public class LeagueCurrentGameParticipant
+    {
+        /// <summary>
+        /// Gets a participant team ID
+        /// </summary>
+        [JsonProperty("teamId")]
+        public int TeamId { get; private set; }
+        /// <summary>
+        /// Gets a participant's summoner spell ID in first slot ([D] key)
+        /// </summary>
+        [JsonProperty("spell1Id")]
+        public int SummonerSpell1Id { get; private set; }
+        /// <summary>
+        /// Gets a participant's summoner spell ID in second slot ([F] key)
+        /// </summary>
+        [JsonProperty("spell2Id")]
+        public int SummonerSpell2Id { get; private set; }
+        /// <summary>
+        /// Gets a participant's champion ID
+        /// </summary>
+        [JsonProperty("championId")]
+        public int ChampionId { get; private set; }
+        /// <summary>
+        /// Gets a participant's profile icon ID
+        /// </summary>
+        [JsonProperty("profileIconId")]
+        public int ProfileIconId { get; private set; }
+        /// <summary>
+        /// Gets <see langword="true"/> if this participant is bot
+        /// </summary>
+        [JsonProperty("bot")]
+        public bool IsBot { get; private set; }
+        /// <summary>
+        /// Gets a participant's summoner name
+        /// </summary>
+        [JsonProperty("summonerName")]
+        public string SummonerName { get; private set; }
+        /// <summary>
+        /// Gets a participant's League Encrypted Summoner ID
+        /// </summary>
+        [JsonProperty("summonerId")]
+        public string SummonerId { get; private set; }
+        /// <summary>
+        /// Gets an array of current participant's game customizations
+        /// </summary>
+        [JsonProperty("gameCustomizationObjects")]
+        public LeagueCurrentGameCustomizationObject[] CustomizationObjects { get; private set; }
+        /// <summary>
+        /// Gets a participant's current selected runes
+        /// </summary>
+        [JsonProperty("perks")]
+        public LeagueCurrentGameParticipantRuneData RuneData { get; private set; }
+    }
+}
---
+++ craftersmine.Riot.Api.League.Spectator/LeagueCurrentGameParticipantRuneData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Spectator
+{
+    /// <summary>
+    /// Represents a currently selected runes in game for participant in League of Legends game
+    /// </summary>
+    public class LeagueCurrentGameParticipantRuneData
+    {
+        /// <summary>
+        /// Gets a selected participant's rune IDs
+        /// </summary>
+        [JsonProperty("oerkIds")]
+        public int[] RuneIds { get; private set; }
+        /// <summary>
+        /// Gets a main rune branch type ID (ex. Resolve, Domination, etc.)
+        /// </summary>
+        [JsonProperty("perkStyle")]
+        public int RuneMainTypeId { get; private set; }
+        /// <summary>
+        /// Gets a secondary rune branch type ID (ex. Resolve, Domination, etc.)
+        /// </summary>
+        [JsonProperty("perkSubStyle")]
+        public int RuneSecondaryTypeId { get; private set; }
+    }
+}
