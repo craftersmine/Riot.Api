641af1b5a55c053619c655d6aa6f48d9837608f4
craftersmine <rodion.morozov.01@gmail.com>
2022-10-26 22:19:52 +0700

    Reorganized attributes in AssemblyInfo

--- craftersmine.Riot.Api.Common/AssemblyInfo.cs
+++ craftersmine.Riot.Api.Common/AssemblyInfo.cs
@@ -1,9 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.Tests")]
+
+[assembly:InternalsVisibleTo("craftersmine.Riot.Api.Status")]
+
+#region League
+
+
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.Account")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Summoner")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.SummonerLeagues")]
@@ -13,6 +19,7 @@ using System.Text;
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Matches")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Matches.Timeline")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Challenges")]
-[assembly:InternalsVisibleTo("craftersmine.Riot.Api.Status")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Tournament")]
 [assembly:InternalsVisibleTo("craftersmine.Riot.Api.League.Spectator")]
+
+#endregion
