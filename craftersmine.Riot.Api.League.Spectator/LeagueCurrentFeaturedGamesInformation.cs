3351f82a2a2b0ae2175717c26e32c9fa594a65fc
craftersmine <rodion.morozov.01@gmail.com>
2022-10-26 21:22:19 +0700

    Added todo comment

--- craftersmine.Riot.Api.League.Spectator/LeagueCurrentFeaturedGamesInformation.cs
+++ craftersmine.Riot.Api.League.Spectator/LeagueCurrentFeaturedGamesInformation.cs
@@ -19,7 +19,7 @@ namespace craftersmine.Riot.Api.League.Spectator
         /// <summary>
         /// Gets a <see cref="TimeSpan"/> interval to wait before next featured games list update
         /// </summary>
-        [JsonProperty("clientRefreshInterval"), JsonConverter(typeof(UnixTimeSpanConverter))]
+        [JsonProperty("clientRefreshInterval"), JsonConverter(typeof(UnixTimeSpanConverter))]  // TODO (craftersmine): determine whether this value is in seconds or milliseconds
         public TimeSpan ClientRefreshInterval { get; private set; }
     }
 }
