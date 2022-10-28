5cfec35ff729093a65bd874c10c8cfcb1baf6e4c
craftersmine <rodion.morozov.01@gmail.com>
2022-10-29 03:13:08 +0700

    Added TFT Match v1 Get match by ID endpoint method

--- craftersmine.Riot.Api.Tft.Matches/RiotTftMatchesApiClient.cs
+++ craftersmine.Riot.Api.Tft.Matches/RiotTftMatchesApiClient.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.Common;
+using craftersmine.Riot.Api.Common.Utils;
 
 namespace craftersmine.Riot.Api.Tft.Matches
 {
-    public class RiotTftMatchesApiClient
+    public class RiotTftMatchesApiClient : RiotApiClient
     {
+        private const string ApiEndpointRoot = "/tft/match/v1/matches";
 
+        public RiotTftMatchesApiClient(RiotApiClientSettings settings) : base(settings) { }
+
+        public async Task<TftMatch> GetMatchByIdAsync(RiotRegion region, string matchId)
+        {
+            if (string.IsNullOrWhiteSpace(matchId))
+                throw new ArgumentNullException(nameof(matchId));
+
+            string endpoint = UriUtils.GetAddress(region, UriUtils.JoinEndpoints(ApiEndpointRoot, matchId));
+
+            TftMatch match = await Client.Get<TftMatch>(endpoint, null);
+            return match;
+        }
     }
 }
