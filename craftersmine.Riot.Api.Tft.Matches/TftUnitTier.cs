ffe6e0f27061c16270ad3c24750c9ab51b792364
craftersmine <rodion.morozov.01@gmail.com>
2022-10-29 03:11:28 +0700

    Added TFT unit tier enum

---
+++ craftersmine.Riot.Api.Tft.Matches/TftUnitTier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Tft.Matches
+{
+    /// <summary>
+    /// Represents Teamfight Tactics unit tier
+    /// </summary>
+    public enum TftUnitTier
+    {
+        /// <summary>
+        /// Tier is unknown
+        /// </summary>
+        Unknown = 0,
+        /// <summary>
+        /// Unit has no tier
+        /// </summary>
+        Default = 1,
+        /// <summary>
+        /// Unit has two stars
+        /// </summary>
+        TwoStars = 2,
+        /// <summary>
+        /// Unit has three stars
+        /// </summary>
+        ThreeStars = 3,
+    }
+}
