813a67e6efa9c1bec5d023da3ec7f0fbf10e64ef
craftersmine <rodion.morozov.01@gmail.com>
2022-10-29 03:11:39 +0700

    Added TFT unit tier enum converter

---
+++ craftersmine.Riot.Api.Tft.Matches/Converters/TftUnitTierConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Tft.Matches.Converters
+{
+    internal class TftUnitTierConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is TftUnitTier unitTier)
+            {
+                writer.WriteValue((int)unitTier);
+                return;
+            }
+
+            writer.WriteValue(0);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Integer)
+                if (reader.Value != null)
+                    return (TftUnitTier) ((long) reader.Value);
+
+            return TftUnitTier.Unknown;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(TftUnitTier);
+        }
+    }
+}
