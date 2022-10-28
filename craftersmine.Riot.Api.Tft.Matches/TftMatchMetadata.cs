8b18233815fd3752f96d44a4897bbfb6f8ddda0d
craftersmine <rodion.morozov.01@gmail.com>
2022-10-28 16:27:07 +0700

    Added TftMatchMetadata class

---
+++ craftersmine.Riot.Api.Tft.Matches/TftMatchMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Tft.Matches
+{
+    /// <summary>
+    /// Represents a Teamfight Tactics match metadata
+    /// </summary>
+    public class TftMatchMetadata
+    {
+        /// <summary>
+        /// Gets current data version of match info
+        /// </summary>
+        [JsonProperty("data_version")]
+        public int DataVersion { get; private set; }
+        /// <summary>
+        /// Gets Teamfight Tactics match ID
+        /// </summary>
+        [JsonProperty("match_id")]
+        public string MatchId { get; private set; }
+        /// <summary>
+        /// Gets an array of <see langword="string"/> of Teamfight Tactics participant PUUIDs
+        /// </summary>
+        [JsonProperty("participants")]
+        public string[] ParticipantIds { get; private set; }
+    }
+}
