6bbef6aa960bfec2583aa89d13bd51cb8f4bbb8a
craftersmine <rodion.morozov.01@gmail.com>
2022-10-28 15:48:07 +0700

    Added TFT League list

---
+++ craftersmine.Riot.Api.Tft.Leagues/TftLeagueList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using craftersmine.Riot.Api.League.SummonerLeagues;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Tft.Leagues
+{
+    /// <summary>
+    /// Represents a Teamfight Tactics Ranked Leagues list
+    /// </summary>
+    public class TftLeagueList : LeagueList
+    {
+        /// <summary>
+        /// Gets current ranked Leagues list entries
+        /// </summary>
+        [JsonProperty("entries")]
+        public new TftSummonerLeague[] Entries { get; private set; }
+    }
+}
