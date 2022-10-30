d0e9e9c30eebb58750366d5e757defd43e246d03
craftersmine <rodion.morozov.01@gmail.com>
2022-10-30 22:31:27 +0700

    Added Valorant player class

---
+++ craftersmine.Riot.Api.Valorant.Ranked/ValorantPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Valorant.Ranked
+{
+    /// <summary>
+    /// Represents a Valorant player
+    /// </summary>
+    public class ValorantPlayer
+    {
+        /// <summary>
+        /// Gets a player's current competitive tier
+        /// </summary>
+        [JsonProperty("competitiveTier")]
+        public int CompetitiveTier { get; private set; }
+        /// <summary>
+        /// Gets a player's current leaderboard rank
+        /// </summary>
+        [JsonProperty("leaderboardRank")]
+        public long LeaderboardRank { get; private set; }
+        /// <summary>
+        /// Gets a player's current ranked rating
+        /// </summary>
+        [JsonProperty("rankedRating")]
+        public long RankedRating { get; private set; }
+        /// <summary>
+        /// Gets a player's total number of wins
+        /// </summary>
+        [JsonProperty("numberOfWins")]
+        public long NumberOfWins { get; private set; }
+        /// <summary>
+        /// Gets a player's Riot Account PUUID
+        /// </summary>
+        /// <remarks>
+        /// This property can be <see langword="null"/> or empty if player is anonymized
+        /// </remarks>
+        [JsonProperty("puuid")]
+        public string Puuid { get; private set; }
+        /// <summary>
+        /// Gets a player's Riot ID name
+        /// </summary>
+        /// <remarks>
+        /// This property can be <see langword="null"/> or empty if player is anonymized
+        /// </remarks>
+        [JsonProperty("gameName")]
+        public string GameName { get; private set; }
+        /// <summary>
+        /// Gets a player's Riot ID tag line
+        /// </summary>
+        /// <remarks>
+        /// This property can be <see langword="null"/> or empty if player is anonymized
+        /// </remarks>
+        [JsonProperty("tagLine")]
+        public string TagLine { get; private set; }
+    }
+}
