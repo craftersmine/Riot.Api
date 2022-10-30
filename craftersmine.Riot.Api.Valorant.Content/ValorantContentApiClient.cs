c3ce6c0008ce6f498b19ad5130c4631debc931f6
craftersmine <rodion.morozov.01@gmail.com>
2022-10-30 18:44:16 +0700

    Added XML docs

--- craftersmine.Riot.Api.Valorant.Content/ValorantContentApiClient.cs
+++ craftersmine.Riot.Api.Valorant.Content/ValorantContentApiClient.cs
@@ -8,12 +8,26 @@ using craftersmine.Riot.Api.Common.Utils;
 
 namespace craftersmine.Riot.Api.Valorant.Content
 {
+    /// <summary>
+    /// Represents Riot Valorant Content v1 API
+    /// </summary>
     public class ValorantContentApiClient : RiotApiClient
     {
         private const string ApiEndpointRoot = "/val/content/v1";
-
+        
+        /// <summary>
+        /// Creates new instance of <see cref="ValorantContentApiClient"/> with specified settings
+        /// </summary>
+        /// <param name="settings">Settings for <see cref="ValorantContentApiClient"/></param>
         public ValorantContentApiClient(RiotApiClientSettings settings) : base(settings) { }
 
+        /// <summary>
+        /// Gets current Valorant Content metadata
+        /// </summary>
+        /// <param name="locale">Filter locale for specified locale</param>
+        /// <returns>Valorant Content metadata</returns>
+        /// <exception cref="ArgumentNullException">When locale is null or empty</exception>
+        /// <exception cref="craftersmine.Riot.Api.Common.Exceptions.RiotApiException">When Riot API request fails</exception>
         public async Task<ValorantContent> GetContentData(string locale)
         {
             if (string.IsNullOrWhiteSpace(locale))
@@ -27,6 +41,7 @@ namespace craftersmine.Riot.Api.Valorant.Content
             return valContent;
         }
 
+        /// <inheritdoc cref="GetContentData(string)"/>
         public async Task<ValorantContent> GetContentData(CultureInfo locale)
         {
             if (locale is null)
@@ -34,6 +49,7 @@ namespace craftersmine.Riot.Api.Valorant.Content
             return await GetContentData(locale.Name);
         }
 
+        /// <inheritdoc cref="GetContentData(string)"/>
         public async Task<ValorantContent> GetContentData()
         {
             string endpoint = UriUtils.GetAddress(Settings.DefaultValorantContentShard,
