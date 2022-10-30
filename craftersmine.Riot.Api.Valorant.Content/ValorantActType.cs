45b484eec8db5ccfa93adbead878104b279e44d7
craftersmine <rodion.morozov.01@gmail.com>
2022-10-30 16:53:58 +0700

    Added Valorant Act Type

---
+++ craftersmine.Riot.Api.Valorant.Content/ValorantActType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Valorant.Content
+{
+    /// <summary>
+    /// Represents a Valorant Act Type
+    /// </summary>
+    public enum ValorantActType
+    {
+        /// <summary>
+        /// It is an act
+        /// </summary>
+        Act,
+        /// <summary>
+        /// It is an episode
+        /// </summary>
+        Episode
+    }
+}
