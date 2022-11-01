c525ecd1d4d5705bae2bc4fd9373a18734b2bc27
craftersmine <rodion.morozov.01@gmail.com>
2022-11-02 02:29:44 +0700

    Added Runeterra match metadata class

---
+++ craftersmine.Riot.Api.Runeterra.Match/RuneterraMatchMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.Runeterra.Matches
+{
+    /// <summary>
+    /// Represents a Legends of Runeterra match metadata
+    /// </summary>
+    public class RuneterraMatchMetadata
+    {
+        /// <summary>
+        /// Gets a Legends or Runeterra match info data version
+        /// </summary>
+        [JsonProperty("data_version")]
+        public int DataVersion { get; private set; }
+        /// <summary>
+        /// Gets a Legends of Runeterra match ID
+        /// </summary>
+        [JsonProperty("match_id")]
+        public Guid MatchId { get; private set; }
+        /// <summary>
+        /// Gets an array of participant's Riot Account PUUIDs that participated in that match
+        /// </summary>
+        [JsonProperty("participants")]
+        public string[] Participants { get; private set; }
+    }
+}
