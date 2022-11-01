c7c8675fba6f3b17d5a4afc2d39b8af6190c10bf
craftersmine <rodion.morozov.01@gmail.com>
2022-11-01 18:18:39 +0700

    Added Runeterra Matches v1 API class

---
+++ craftersmine.Riot.Api.Runeterra.Match/RuneterraMatchesApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using craftersmine.Riot.Api.Common;
+
+namespace craftersmine.Riot.Api.Runeterra.Match
+{
+    public class RuneterraMatchesApiClient : RiotApiClient
+    {
+        public RuneterraMatchesApiClient(RiotApiClientSettings settings) : base(settings) { }
+    }
+}
