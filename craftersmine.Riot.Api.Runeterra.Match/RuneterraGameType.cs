cc999925ac2679ecb23e5eb48680e8c57aa4e8a7
craftersmine <rodion.morozov.01@gmail.com>
2022-11-03 20:48:52 +0700

    Added RuneterraGameType enum

---
+++ craftersmine.Riot.Api.Runeterra.Match/RuneterraGameType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.Runeterra.Matches
+{
+    /// <summary>
+    /// Represents a Legends of Runeterra game type
+    /// </summary>
+    public enum RuneterraGameType
+    {
+        /// <summary>
+        /// Ranked game
+        /// </summary>
+        Ranked,
+        /// <summary>
+        /// Normal game
+        /// </summary>
+        Normal,
+        /// <summary>
+        /// Against the bot
+        /// </summary>
+        AI,
+        /// <summary>
+        /// Tutorial game
+        /// </summary>
+        Tutorial,
+        /// <summary>
+        /// Trial Vanilla game
+        /// </summary>
+        VanillaTrial,
+        /// <summary>
+        /// Singleton Gauntlet game
+        /// </summary>
+        Singleton,
+        /// <summary>
+        /// Standard Gauntlet game
+        /// </summary>
+        StandardGauntlet
+    }
+}
