f868a060055cf0544d36d92159bd16680b8ea287
craftersmine <rodion.morozov.01@gmail.com>
2022-11-05 18:26:32 +0700

    Added League active player data

---
+++ craftersmine.Riot.Api.League.Client/LeagueActivePlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Client
+{
+    public class LeagueActivePlayerData
+    {
+        public int ChampiontLevel { get; private set; }
+        public bool HasRelativeTeamColors { get; private set; }
+        public double CurrentGold { get; private set; }
+        public string SummonerName { get; private set; }
+        public LeagueChampionAbilities Abilities { get; private set; }
+        public LeagueChampionStats ChampionStats { get; private set; }
+        public LeaguePlayerRunes Runes { get; private set; }
+    }
+}
