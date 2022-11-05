866614384a9723820ef978324b691f680b0f7fff
craftersmine <rodion.morozov.01@gmail.com>
2022-11-05 18:25:32 +0700

    Added local League Client API project

---
+++ craftersmine.Riot.Api.League.Client/LeagueClientApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.Common;
+
+namespace craftersmine.Riot.Api.League.Client
+{
+    public class LeagueLocalApiClient : RiotApiClient
+    {
+        public LeagueLocalApiClient(RiotApiClientSettings settings) : base(settings) { }
+
+        public async Task<LeagueGameData> GetAllGameDataAsync()
+        {
+
+        }
+    }
+}
