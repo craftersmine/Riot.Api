9136567ff10b790b19bb84873513b8536d814e55
craftersmine <rodion.morozov.01@gmail.com>
2022-11-05 18:25:46 +0700

    Added League game data class

---
+++ craftersmine.Riot.Api.League.Client/LeagueGameData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Client
+{
+    public class LeagueGameData
+    {
+        public LeagueActivePlayerData ActivePlayer { get; private set; }
+        public LeaguePlayerData[] Players { get; private set; }
+        public LeagueEvent[] Events { get; private set; }
+        public LeagueGameMetadata GameData { get; private set; }
+    }
+}
