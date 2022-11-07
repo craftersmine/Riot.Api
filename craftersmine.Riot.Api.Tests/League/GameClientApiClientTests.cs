3749c3e5e7c9e3f29bb04888b4daf22a1b50194f
craftersmine <rodion.morozov.01@gmail.com>
2022-11-07 23:56:05 +0700

    Added Game Client tests

---
+++ craftersmine.Riot.Api.Tests/League/GameClientApiClientTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.Common;
+using craftersmine.Riot.Api.League.Client;
+
+namespace craftersmine.Riot.Api.Tests.League
+{
+    [TestClass]
+    public class GameClientApiClientTests
+    {
+        public LeagueGameClientApiClient? Client { get; set; }
+        public string? ApiKey { get; set; }
+
+        public TestContext? TestContext { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ApiKey = TestContext?.Properties["ApiKey"]?.ToString();
+            if (string.IsNullOrWhiteSpace(ApiKey))
+                Assert.Fail("No Riot API key provided!");
+            Client = new LeagueGameClientApiClient(RiotApiClientSettingsBuilder.CreateSettingsBuilder(ApiKey).Build());
+        }
+
+        [TestMethod]
+        public async Task GetAllGameDataTests()
+        {
+            Assert.IsNotNull(Client, "Client is not initialized");
+            LeagueGameData gameData = await Client.GetAllGameDataAsync();
+        }
+    }
+}
