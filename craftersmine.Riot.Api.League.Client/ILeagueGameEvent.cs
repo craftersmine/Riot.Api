334b6f73cf27ac986b809c35023a501560c47e61
craftersmine <rodion.morozov.01@gmail.com>
2022-11-07 23:58:19 +0700

    Added League game event interface

---
+++ craftersmine.Riot.Api.League.Client/ILeagueGameEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Client
+{
+    internal interface ILeagueGameEvent
+    {
+        int EventId { get; }
+        TimeSpan EventTime { get; }
+        LeagueGameEventType EventType { get; }
+    }
+}
