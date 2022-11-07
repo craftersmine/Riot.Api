a594f039beeee009b4f07b6ec6c8ece6646d9f87
craftersmine <rodion.morozov.01@gmail.com>
2022-11-08 00:00:07 +0700

    Added missing attributes

--- craftersmine.Riot.Api.League.Client/LeagueActivePlayerData.cs
+++ craftersmine.Riot.Api.League.Client/LeagueActivePlayerData.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace craftersmine.Riot.Api.League.Client
 {
@@ -12,25 +13,32 @@ namespace craftersmine.Riot.Api.League.Client
         /// <summary>
         /// Gets player's champion level
         /// </summary>
+        [JsonProperty("level")]
+        public int ChampionLevel { get; private set; }
         /// <summary>
         /// Gets <see langword="true"/> if player has enabled relative team colors
         /// </summary>
+        [JsonProperty("teamRelativeColors")]
         public bool HasRelativeTeamColors { get; private set; }
         /// <summary>
         /// Gets current player's gold value in store
         /// </summary>
+        [JsonProperty("currentGold")]
         public double CurrentGold { get; private set; }
         /// <summary>
         /// Gets current player's summoner name
         /// </summary>
+        [JsonProperty("summonerName")]
         public string SummonerName { get; private set; }
         /// <summary>
         /// Gets current player's champion abilities
         /// </summary>
+        [JsonProperty("abilities")]
         public LeagueChampionAbilities Abilities { get; private set; }
         /// <summary>
         /// Gets current player's champion stats
         /// </summary>
+        [JsonProperty("championStats")]
         public LeagueChampionStats ChampionStats { get; private set; }
         /// <summary>
         /// Gets current player's selected runes
