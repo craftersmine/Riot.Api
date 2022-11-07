2fa3925496ebff89869637f5ff342cb370a4c798
craftersmine <rodion.morozov.01@gmail.com>
2022-11-07 23:57:25 +0700

    Added League game event type

---
+++ craftersmine.Riot.Api.League.Client/LeagueGameEventType.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Riot.Api.League.Client
+{
+    /// <summary>
+    /// Represents a League of Legends game event
+    /// </summary>
+    public enum LeagueGameEventType
+    {
+        /// <summary>
+        /// Game has started
+        /// </summary>
+        GameStart,
+        /// <summary>
+        /// Minions started spawning
+        /// </summary>
+        MinionsSpawning,
+        /// <summary>
+        /// First kill made
+        /// </summary>
+        FirstBrick,
+        /// <summary>
+        /// Turret has been destroyed
+        /// </summary>
+        TurretKilled,
+        /// <summary>
+        /// Inhibitor has been destroyed
+        /// </summary>
+        InhibKilled,
+        /// <summary>
+        /// Dragon has been slain
+        /// </summary>
+        DragonKill,
+        /// <summary>
+        /// Herald has been slain
+        /// </summary>
+        HeraldKill,
+        /// <summary>
+        /// Baron has been slain
+        /// </summary>
+        BaronKill,
+        /// <summary>
+        /// Champion has been slain
+        /// </summary>
+        ChampionKill,
+        /// <summary>
+        /// Multikill has been performed
+        /// </summary>
+        Multikill,
+        /// <summary>
+        /// Team has been aced
+        /// </summary>
+        Ace
+    }
+}
