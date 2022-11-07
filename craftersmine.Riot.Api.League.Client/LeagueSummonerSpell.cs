e10fd10c43fbf8586f1197a47827af7c329103d5
craftersmine <rodion.morozov.01@gmail.com>
2022-11-07 23:56:24 +0700

    Added League summoner spell class

---
+++ craftersmine.Riot.Api.League.Client/LeagueSummonerSpell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Client
+{
+    /// <summary>
+    /// Represents a League of Legends summoner spell
+    /// </summary>
+    public class LeagueSummonerSpell
+    {
+        /// <summary>
+        /// Gets a localized name of summoner spell
+        /// </summary>
+        [JsonProperty("displayName")]
+        public string DisplayName { get; private set; }
+        /// <summary>
+        /// Gets an internal raw description of summoner spell
+        /// </summary>
+        [JsonProperty("rawDescription")]
+        public string RawDescription { get; private set; }
+        /// <summary>
+        /// Gets an internal raw display name of summoner spell
+        /// </summary>
+        [JsonProperty("rawDisplayName")]
+        public string RawDisplayName { get; private set; }
+    }
+}
