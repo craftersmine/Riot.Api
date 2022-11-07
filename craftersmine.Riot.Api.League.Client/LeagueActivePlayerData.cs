a86fada6b22b0e71be98ce28f1776311f6f3485f
craftersmine <rodion.morozov.01@gmail.com>
2022-11-07 23:59:31 +0700

    Added XML docs

--- craftersmine.Riot.Api.League.Client/LeagueActivePlayerData.cs
+++ craftersmine.Riot.Api.League.Client/LeagueActivePlayerData.cs
@@ -1,16 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace craftersmine.Riot.Api.League.Client
 {
+    /// <summary>
+    /// Represents a League of Legends player that currently has running League Game Client
+    /// </summary>
     public class LeagueActivePlayerData
     {
-        public int ChampiontLevel { get; private set; }
+        /// <summary>
+        /// Gets player's champion level
+        /// </summary>
+        /// <summary>
+        /// Gets <see langword="true"/> if player has enabled relative team colors
+        /// </summary>
         public bool HasRelativeTeamColors { get; private set; }
+        /// <summary>
+        /// Gets current player's gold value in store
+        /// </summary>
         public double CurrentGold { get; private set; }
+        /// <summary>
+        /// Gets current player's summoner name
+        /// </summary>
         public string SummonerName { get; private set; }
+        /// <summary>
+        /// Gets current player's champion abilities
+        /// </summary>
         public LeagueChampionAbilities Abilities { get; private set; }
+        /// <summary>
+        /// Gets current player's champion stats
+        /// </summary>
         public LeagueChampionStats ChampionStats { get; private set; }
         public LeaguePlayerRunes Runes { get; private set; }
     }
