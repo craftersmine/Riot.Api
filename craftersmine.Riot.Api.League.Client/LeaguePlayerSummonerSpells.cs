594ff9b93cdbb1113b0174bfd031d22ace3f953c
craftersmine <rodion.morozov.01@gmail.com>
2022-11-07 23:57:04 +0700

    Added League player's summoner spell set class

---
+++ craftersmine.Riot.Api.League.Client/LeaguePlayerSummonerSpells.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Client
+{
+    /// <summary>
+    /// Represents a League of Legends player's summoner spell set
+    /// </summary>
+    public class LeaguePlayerSummonerSpells
+    {
+        /// <summary>
+        /// Gets a first summoner spell information ([D] key)
+        /// </summary>
+        [JsonProperty("summonerSpellOne")]
+        public LeagueSummonerSpell SpellOne { get; private set; }
+        /// <summary>
+        /// Gets a second summoner spell information ([F] key)
+        /// </summary>
+        [JsonProperty("summonerSpellTwo")]
+        public LeagueSummonerSpell SpellTwo { get; private set; }
+    }
+}
