01d1578fb580d41566c05b61169465bdae093dad
craftersmine <rodion.morozov.01@gmail.com>
2023-03-05 03:49:22 +0700

    Moved attributes from base class to interface

--- craftersmine.Riot.Api.League.Client/ILeagueGameEvent.cs
+++ craftersmine.Riot.Api.League.Client/ILeagueGameEvent.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using craftersmine.Riot.Api.Common.Converters;
+
+using Newtonsoft.Json;
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace craftersmine.Riot.Api.League.Client
 {
-    internal interface ILeagueGameEvent
+    /// <summary>
+    /// Represents a League game event
+    /// </summary>
+    public interface ILeagueGameEvent
     {
+        /// <summary>
+        /// Gets an ID of event
+        /// </summary>
+        [JsonProperty("EventID")]
         int EventId { get; }
+        /// <summary>
+        /// Gets a timestamp when event was occurred relative to game start
+        /// </summary>
+        [JsonProperty("EventTime"), JsonConverter(typeof(UnixTimeSpanConverter), true)]
         TimeSpan EventTime { get; }
+        /// <summary>
+        /// Gets a type of event
+        /// </summary>
+        [JsonProperty("EventName")]
         LeagueGameEventType EventType { get; }
     }
 }
--- craftersmine.Riot.Api.League.Client/LeagueBaseGameEvent.cs
+++ craftersmine.Riot.Api.League.Client/LeagueBaseGameEvent.cs
@@ -14,17 +14,14 @@ namespace craftersmine.Riot.Api.League.Client
         /// <summary>
         /// Gets event index in event timeline
         /// </summary>
-        [JsonProperty("EventID")]
         public int EventId { get; private set; }
         /// <summary>
         /// Gets <see cref="TimeSpan"/> timestamp when this event has occurred
         /// </summary>
-        [JsonProperty("EventTime"), JsonConverter(typeof(UnixTimeSpanConverter), true)]
         public TimeSpan EventTime { get; private set; }
         /// <summary>
         /// Gets a type of event
         /// </summary>
-        [JsonProperty("EventType")]
         public LeagueGameEventType EventType { get; private set; }
     }
 }
