e5da979b3ea698e8220b042d76b235d2feea5f44
craftersmine <rodion.morozov.01@gmail.com>
2023-03-05 23:07:16 +0700

    Added League Replay API project

---
+++ craftersmine.Riot.Api.League.Client.Replay/LeagueReplayApiClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.Common;
+using craftersmine.Riot.Api.Common.Utils;
+
+namespace craftersmine.Riot.Api.League.Client.Replay
+{
+    public class LeagueReplayApiClient : RiotApiClient
+    {
+        protected const string LeagueClientRoot = "https://localhost:2999/replay/";
+
+        /// <summary>
+        /// Creates a new instance of <see cref="LeagueReplayApiClient"/>
+        /// </summary>
+        /// <param name="settings">Settings for <see cref="LeagueReplayApiClient"/>. IgnoreSSLCertificates is required to be enabled!</param>
+        public LeagueReplayApiClient(RiotApiClientSettings settings) : base(settings)
+        { }
+
+        public async Task<LeagueGameInfo> GetGameInfoAsync()
+        {
+            string endpoint = UriUtils.GetAddress(LeagueClientRoot, "game");
+
+            LeagueGameInfo gameInfo = await Client.GetAsync<LeagueGameInfo>(endpoint, null);
+            return gameInfo;
+        }
+    }
+}
---
+++ craftersmine.Riot.Api.Tests/League/GameReplayApiTests.cs
@@ -0,0 +1,37 @@
+﻿using craftersmine.Riot.Api.Common;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.Riot.Api.League.Client.Replay;
+
+namespace craftersmine.Riot.Api.Tests.League
+{
+    [TestClass]
+    public class GameReplayApiTests
+    {
+        public LeagueReplayApiClient? Client { get; set; }
+        public string? ApiKey { get; set; }
+
+        public TestContext? TestContext { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ApiKey = TestContext?.Properties["ApiKey"]?.ToString();
+            if (string.IsNullOrWhiteSpace(ApiKey))
+                Assert.Fail("No Riot API key provided!");
+            Client = new LeagueReplayApiClient(RiotApiClientSettingsBuilder.CreateSettingsBuilder(ApiKey).IgnoreSSLCertificates().Build());
+        }
+
+        [TestMethod]
+        public async Task GetGameInfoTests()
+        {
+            Assert.IsNotNull(Client, "Client is not initialized");
+
+            LeagueGameInfo gameInfo = await Client.GetGameInfoAsync();
+        }
+    }
+}
