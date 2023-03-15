1ea51ab204d854249cb011f99855a3f8eb869f16
craftersmine <rodion.morozov.01@gmail.com>
2023-03-15 16:10:01 +0700

    Added game replay particles test

--- craftersmine.Riot.Api.Tests/League/GameReplayApiTests.cs
+++ craftersmine.Riot.Api.Tests/League/GameReplayApiTests.cs
@@ -33,5 +33,18 @@ namespace craftersmine.Riot.Api.Tests.League
 
             LeagueGameInfo gameInfo = await Client.GetGameInfoAsync();
         }
+
+        [TestMethod]
+        public async Task ParticlesTests()
+        {
+            Assert.IsNotNull(Client, "Client is not initialized");
+
+            LeagueGameParticlesCollection particles = await Client.GetGameParticlesAsync();
+            particles["Ornn_Base_W_Debuff_hit"] = false;
+            LeagueGameParticlesCollection particlesUpdated = await Client.SetGameParticlesAsync(particles);
+
+            bool newVal = await Client.SetGameParticleVisibilityAsync("Ornn_Base_W_Debuff_hit_avatar", true);
+            bool Ornn_Base_W_Debuff_icon = await Client.GetGameParticleVisibilityAsync("Ornn_Base_W_Debuff_icon");
+        }
     }
 }
