30b759c8a30cadfc2a71a845240b194118f630ba
craftersmine <rodion.morozov.01@gmail.com>
2023-03-15 16:08:23 +0700

    Added Game Particles collection

---
+++ craftersmine.Riot.Api.League.Client.Replay/LeagueGameParticlesCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.Riot.Api.League.Client.Replay
+{
+    public class LeagueGameParticlesCollection : Dictionary<string, bool>
+    {
+        
+    }
+}
