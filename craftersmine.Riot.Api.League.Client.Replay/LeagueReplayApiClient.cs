bdeee21e5af1537739047ce84ff631853792414f
craftersmine <rodion.morozov.01@gmail.com>
2023-03-15 16:13:32 +0700

    Added few replay endpoints methods

--- craftersmine.Riot.Api.League.Client.Replay/LeagueReplayApiClient.cs
+++ craftersmine.Riot.Api.League.Client.Replay/LeagueReplayApiClient.cs
@@ -23,5 +23,64 @@ namespace craftersmine.Riot.Api.League.Client.Replay
             LeagueGameInfo gameInfo = await Client.GetAsync<LeagueGameInfo>(endpoint, null);
             return gameInfo;
         }
+
+        public async Task<LeagueGameParticlesCollection> GetGameParticlesAsync()
+        {
+            string endpoint = UriUtils.GetAddress(LeagueClientRoot, "particles");
+
+            LeagueGameParticlesCollection particles =
+                await Client.GetAsync<LeagueGameParticlesCollection>(endpoint, null);
+            return particles;
+        }
+
+        public async Task<LeagueGameParticlesCollection> SetGameParticlesAsync(LeagueGameParticlesCollection particles)
+        {
+            string endpoint = UriUtils.GetAddress(LeagueClientRoot, "particles");
+            LeagueGameParticlesCollection ptcls = await Client.PostAsync<LeagueGameParticlesCollection>(endpoint, null, particles);
+            return ptcls;
+        }
+
+        public async Task<bool> SetGameParticleVisibilityAsync(string particleId, bool visibilityValue)
+        {
+            LeagueGameParticlesCollection particles = await GetGameParticlesAsync();
+            if (!particles.ContainsKey(particleId))
+                throw new ArgumentException("Particle ID is not found in game!", nameof(particleId));
+
+            particles[particleId] = visibilityValue;
+            particles = await SetGameParticlesAsync(particles);
+            return particles[particleId];
+        }
+
+        public async Task<bool> GetGameParticleVisibilityAsync(string particleId)
+        {
+            LeagueGameParticlesCollection particles = await GetGameParticlesAsync();
+            if (!particles.ContainsKey(particleId))
+                throw new ArgumentException("Particle ID is not found in game!", nameof(particleId));
+
+            return particles[particleId];
+        }
+
+        public async Task<LeagueReplayPlaybackSettings> GetReplayPlaybackSettingsAsync()
+        {
+            string endpoint = UriUtils.GetAddress(LeagueClientRoot, "playback");
+
+            LeagueReplayPlaybackSettings settings = await Client.GetAsync<LeagueReplayPlaybackSettings>(endpoint, null);
+            return settings;
+        }
+
+        public async Task<LeagueReplayPlaybackSettings> SetReplayPlaybackSettingsAsync(
+            LeagueReplayPlaybackSettings settings)
+        {
+            if (settings is null)
+                throw new ArgumentNullException(nameof(settings));
+
+            string endpoint = UriUtils.GetAddress(LeagueClientRoot, "playback");
+
+            LeagueReplayPlaybackSettings settingsNew =
+                await Client.PostAsync<LeagueReplayPlaybackSettings>(endpoint, null, settings);
+            return settingsNew;
+        }
+
+        
     }
 }
